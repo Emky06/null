f93f8fde4a25313d35e8882be7441785be825b85
AxtralWiZaRd <134928023+AxtralWiZaRd@users.noreply.github.com>
2025-09-24 16:15:46 +0200

    Update owner-restart.js

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -1,47 +1,20 @@
-import { spawn } from 'child_process'
-
-const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms))
-
-function isAuthorized(sender) {
-    const authorizedNumbers = [
-        '393755435365@s.whatsapp.net',
-        '393511198848@s.whatsapp.net',
-        '35796261367@s.whatsapp.net',
-        '35795191323@s.whatsapp.net',
-        '573161874043@s.whatsapp.net',
-    ];
-    return authorizedNumbers.includes(sender);
-}
-
-let handler = async (m, { conn }) => {
-    if (!isAuthorized(m.sender)) {
-        await conn.sendMessage(m.chat, { text: "❌ Non sei autorizzato a usare questo comando." });
-        return;
+const handler = async (m, { conn, isROwner, text }) => {
+    const datas = global
+    
+    if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
+    const { key } = await conn.sendMessage(m.chat, {text: `Sto riavviando`}, {quoted: m})
+    await delay(1000 * 1)
+    await conn.sendMessage(m.chat, {text: `🚀🚀🚀🚀`, edit: key})
+    await delay(1000 * 1)
+    await conn.sendMessage(m.chat, {text: `🚀🚀🚀🚀🚀🚀`, edit: key})
+    await conn.sendMessage(m.chat, {text: `Riavviato con successo`, edit: key})
+    //process.send('reset')
+    process.exit(0); 
     }
-
-    if (!process.send) throw 'Non fare: node main.js\nFai: node index.js';
-
-    if (global.conn.user.jid === conn.user.jid) {
-        const sentMsg = await conn.sendMessage(m.chat, { text: `*Sto riavviando...⏳*` }, { quoted: m });
-
-        await delay(1000);
-        await conn.sendMessage(m.chat, { text: `🚀🚀🚀🚀`, edit: sentMsg.key });
-
-        await delay(1000);
-        await conn.sendMessage(m.chat, { text: `🚀🚀🚀🚀🚀🚀`, edit: sentMsg.key });
-
-        await delay(1000);
-        await conn.sendMessage(m.chat, { text: `*Riavviato con successo✅*`, edit: sentMsg.key });
-
-        process.send('reset');
-    } else {
-        throw '_eeeeeiiittsssss..._';
-    }
-}
-
-handler.help = ['restart', 'riavvia', 'reiniciar'];
-handler.tags = ['proprietario'];
-handler.command = /^(res(tart)?|riavvia|reiniciar)$/i;
-handler.rowner = true;
-
-export default handler;
+    handler.help = ['riavvia'] 
+    handler.tags = ['owner']
+    handler.command = ['riavvia','reiniciar'] 
+    handler.owner = true
+    export default handler
+    
+    const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
