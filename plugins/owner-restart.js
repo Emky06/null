ee557bdcf7a937c24229e0230368e00c1f76ef31
AxtralWiZaRd <134928023+AxtralWiZaRd@users.noreply.github.com>
2025-09-24 16:19:34 +0200

    Update owner-restart.js

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -2,18 +2,18 @@ const handler = async (m, { conn, isROwner, text }) => {
     const datas = global
     
     if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
-    const { key } = await conn.sendMessage(m.chat, {text: `Sto riavviando`}, {quoted: m})
+    const { key } = await conn.sendMessage(m.chat, {text: `*Sto riavviando...⏳*`}, {quoted: m})
     await delay(1000 * 1)
     await conn.sendMessage(m.chat, {text: `🚀🚀🚀🚀`, edit: key})
     await delay(1000 * 1)
     await conn.sendMessage(m.chat, {text: `🚀🚀🚀🚀🚀🚀`, edit: key})
-    await conn.sendMessage(m.chat, {text: `Riavviato con successo`, edit: key})
+    await conn.sendMessage(m.chat, {text: `*Riavviato con successo ✅*`, edit: key})
     //process.send('reset')
     process.exit(0); 
     }
     handler.help = ['riavvia'] 
     handler.tags = ['owner']
-    handler.command = ['riavvia','reiniciar'] 
+    handler.command = ['riavvia','restart'] 
     handler.owner = true
     export default handler
     
