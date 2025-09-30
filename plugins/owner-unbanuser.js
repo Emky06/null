32e5d841bc3ff5d08c8c48b68dcff2a0ac523ae6
AxtralWiZaRd <134928023+AxtralWiZaRd@users.noreply.github.com>
2025-09-30 23:05:03 +0200

    Update owner-unbanuser.js

--- plugins/owner-unbanuser.js
+++ plugins/owner-unbanuser.js
@@ -1,31 +1,39 @@
 import fs from 'fs/promises';
 
-async function handler(m, { conn, text }) {
-  if (!text) return;
-  let target;
-  if (m.isGroup) {
-    target = m.mentionedJid?.[0];
-  } else {
-    target = m.chat;
-  }
-  if (!target) return;
+let handler = async (message, { conn, text }) => {
+    if (!text && !message.mentionedJid?.[0] && !message.quoted) {
+        return conn.reply(message.chat, '❗ Tagga, rispondi o scrivi il numero (es: 3934xxxxxxx)', message);
+    }
 
-  global.db.data.users[target].banned = false;
+    let target;
 
-  // Usa solo file locale
-  const thumbnail = await fs.readFile('icone/unbanuser.png');
+    if (message.mentionedJid?.[0]) {
+        target = message.mentionedJid[0];
+    } else if (message.quoted) {
+        target = message.quoted.sender;
+    } else if (text) {
+        let number = text.replace(/\D/g, '');
+        if (number.length < 8) return conn.reply(message.chat, '❗ Numero non valido.', message);
+        target = number + '@s.whatsapp.net';
+    }
 
-  const vcardMessage = {
-    key: {
-      participants: "0@s.whatsapp.net",
-      fromMe: false,
-      id: "Halo"
-    },
-    message: {
-      locationMessage: {
-        name: "𝐔𝐭𝐞𝐧𝐭𝐞 𝐬𝐛𝐥𝐨𝐜𝐜𝐚𝐭𝐨",
-        jpegThumbnail: thumbnail,
-        vcard: `BEGIN:VCARD
+    let users = global.db.data.users;
+    if (!users[target]) users[target] = {};
+    users[target].banned = false;
+
+    const thumbnail = await fs.readFile('icone/unbanuser.png');
+
+    let fakeMsg = {
+        key: {
+            participants: "0@s.whatsapp.net",
+            fromMe: false,
+            id: "Halo"
+        },
+        message: {
+            locationMessage: {
+                name: "𝐔𝐭𝐞𝐧𝐭𝐞 𝐬𝐛𝐥𝐨𝐜𝐜𝐚𝐭𝐨",
+                jpegThumbnail: thumbnail,
+                vcard: `BEGIN:VCARD
 VERSION:3.0
 N:;Unlimited;;;
 FN:Unlimited
@@ -36,16 +44,21 @@ item1.X-ABLabel:Unlimited
 X-WA-BIZ-DESCRIPTION:ofc
 X-WA-BIZ-NAME:Unlimited
 END:VCARD`
-      }
-    },
-    participant: "0@s.whatsapp.net"
-  };
+            }
+        },
+        participant: "0@s.whatsapp.net"
+    };
 
-  conn.reply(m.chat, "𝐐𝐮𝐞𝐬𝐭𝐨 𝐮𝐭𝐞𝐧𝐭𝐞 𝐩𝐨𝐭𝐫𝐚̀ 𝐞𝐬𝐞𝐠𝐮𝐢𝐫𝐞 𝐝𝐢 𝐧𝐮𝐨𝐯𝐨 𝐢 𝐜𝐨𝐦𝐚𝐧𝐝𝐢.", vcardMessage);
-}
+    conn.reply(
+        message.chat,
+        "𝐐𝐮𝐞𝐬𝐭𝐨 𝐮𝐭𝐞𝐧𝐭𝐞 𝐩𝐨𝐭𝐫à 𝐞𝐬𝐞𝐠𝐮𝐢𝐫𝐞 𝐝𝐢 𝐧𝐮𝐨𝐯𝐨 𝐢 𝐜𝐨𝐦𝐚𝐧𝐝𝐢.",
+        fakeMsg
+    );
+};
 
 handler.help = ['unbanuser'];
 handler.tags = ['help'];
 handler.command = /^unbanuser|unban$/i;
 handler.owner = true;
+
 export default handler;
