cee7225d861f20754744c57c4e0e5ba096783004
AxtralWiZaRd <134928023+AxtralWiZaRd@users.noreply.github.com>
2025-10-10 03:18:39 +0200

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ global.confirmCode = ""
 global.owner = [
     ['393512884684', 'Owner', true],
     ['393335608801', 'Kinderino', true],
-    ['393508571301', 'Riad', true],
+    ['393793178438', 'Riad', true],
     ['393926119886', 'Vero', true],
     ['35795191323', 'Owner', true],
     ['35796261367', 'Owner', true],
