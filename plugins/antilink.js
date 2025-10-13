c2f5160c419ff5554e4eec8c70d5a16d241fa241
AxtralWiZaRd <134928023+AxtralWiZaRd@users.noreply.github.com>
2025-10-13 21:09:02 +0200

    Update antilink.js

--- plugins/antilink.js
+++ plugins/antilink.js
@@ -195,9 +195,9 @@ END:VCARD`
     } else {
         user.warn = 0
         user.warnReasons = []
-        await conn.sendMessage(m.chat, { 
-  text: '⛔ 𝐔𝐓𝐄𝐍𝐓𝐄 𝐑𝐈𝐌𝐎𝐒𝐒𝐎 𝐃𝐎𝐏𝐎 𝟑 𝐀𝐕𝐕𝐄𝐑𝐓𝐈𝐌𝐄𝐍𝐓𝐈'
-});
-        await conn.groupParticipantsUpdate(msg.chat, [msg.sender], 'remove')
+        await conn.sendMessage(msg.chat, { 
+    text: '⛔ 𝐔𝐓𝐄𝐍𝐓𝐄 𝐑𝐈𝐌𝐎𝐒𝐒𝐎 𝐃𝐎𝐏𝐎 𝟑 𝐀𝐕𝐕𝐄𝐑𝐓𝐈𝐌𝐄𝐍𝐓𝐈' 
+})
+await conn.groupParticipantsUpdate(msg.chat, [msg.sender], 'remove')
     }
 }
