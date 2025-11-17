479d8682549b039b9c7caf5356bf947182fa741c
AxtralWiZaRd <134928023+AxtralWiZaRd@users.noreply.github.com>
2025-11-17 22:42:14 +0100

    Update handler.js

--- handler.js
+++ handler.js
@@ -729,7 +729,7 @@ export async function participantsUpdate({ id, participants, action }) {
                                     "previewType": "PHOTO", 
                                     "thumbnailUrl": ``, 
                                     "thumbnail": apii.data,
-                                    "mediaType": 1
+                                    "mediaType": 1,
                                     "renderLargerThumbnail": false
                                 }
                             }
