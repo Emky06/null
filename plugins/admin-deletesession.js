5d12e8b0bc7dc94f0c5fef0c7b0bf9260c343209
AxtralWiZaRd <134928023+AxtralWiZaRd@users.noreply.github.com>
2025-11-26 21:12:48 +0100

    Update admin-deletesession.js

--- plugins/admin-deletesession.js
+++ plugins/admin-deletesession.js
@@ -85,7 +85,7 @@ const handler = async (m, { conn, usedPrefix }) => {
 
 handler.help = ['del_reg_in_session_owner'];
 handler.tags = ["owner"];
-handler.command = /^(dsbura|ds)$/i;
+handler.command = /^(diosbura|ds)$/i;
 handler.admin = true;
 
 export default handler;
