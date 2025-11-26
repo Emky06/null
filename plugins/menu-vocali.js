727da91cc735a622ac2525900e88356ef58c9315
AxtralWiZaRd <134928023+AxtralWiZaRd@users.noreply.github.com>
2025-11-26 19:26:24 +0100

    Update menu-vocali.js

--- plugins/menu-vocali.js
+++ plugins/menu-vocali.js
@@ -39,8 +39,8 @@ const handler = async (message, { conn, usedPrefix }) => {
 ➤ ${usedPrefix}𝐩𝐨𝐥𝐢𝐳𝐢𝐚
 ➤ ${usedPrefix}𝐭𝐫𝐚𝐥𝐥𝐚𝐥𝐥𝐞𝐫𝐨
 ─────────────────────
-𝑽𝒆𝒓𝒔𝒊𝒐𝒏𝒆: ${vs}
-𝐁𝐲 ${nomebot}
+*𝑽𝒆𝒓𝒔𝒊𝒐𝒏𝒆: ${vs}*
+*𝐁𝐲 ${nomebot}*
 `.trim();
 
     await conn.sendMessage(message.chat, {
