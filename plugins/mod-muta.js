06aecb194f2db4a7766088e37584af3e7b894b3c
AxtralWiZaRd <134928023+AxtralWiZaRd@users.noreply.github.com>
2025-12-07 18:53:17 +0100

    Update mod-muta.js

--- plugins/mod-muta.js
+++ plugins/mod-muta.js
@@ -27,7 +27,7 @@ const handler = async (msg, { conn, command, text, isPrems }) => {
 
   const protectedNumbers = [
     '393512884684@s.whatsapp.net',
-    '35795191323@s.whatsapp.net',
+    '393401431923@s.whatsapp.net',
   ];
 
   // Inizializza l'utente se non esiste
