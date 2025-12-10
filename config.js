0d32bd992d65a42afb85ecdb581a4e10925e740e
AxtralWiZaRd <134928023+AxtralWiZaRd@users.noreply.github.com>
2025-12-10 18:47:28 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ global.botnumber = ""
 global.confirmCode = ""
 
 global.owner = [
-    ['40731383635', 'Owner', true],
+    ['4915213333375', 'Owner', true],
     ['393926119886', 'Vero', true],
     ['573171519160', 'Vero voip', true],
     ['393511198848', 'Owner', true],
