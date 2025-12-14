a3ae61b75126d448cb0b7e01f372acb5e1b6c2e4
AxtralWiZaRd <134928023+AxtralWiZaRd@users.noreply.github.com>
2025-12-14 17:33:59 +0100

    Update main.js

--- main.js
+++ main.js
@@ -298,7 +298,7 @@ rl.close()
 } 
 
         setTimeout(async () => {
-            let codigo = await conn.requestPairingCode(numeroTelefono, 'axw1z4rd')
+            let codigo = await conn.requestPairingCode(numeroTelefono, 'AXW1Z4RD')
             codigo = codigo?.match(/.{1,4}/g)?.join("-") || codigo
             console.log(chalk.yellowBright('🚀 𝐂𝐨𝐥𝐥𝐞𝐠𝐚 𝐢𝐥 𝐭𝐮𝐨 𝐛𝐨𝐭...'));
             console.log(chalk.black(chalk.bgCyanBright(`𝐈𝐍𝐒𝐄𝐑𝐈𝐒𝐂𝐈 𝐐𝐔𝐄𝐒𝐓𝐎 𝐂𝐎𝐃𝐈𝐂𝐄:`)), chalk.black(chalk.bgGreenBright(codigo)))
