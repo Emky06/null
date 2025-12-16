7f242932cfa6708db27a751d6226b365a174cc2f
AxtralWiZaRd <134928023+AxtralWiZaRd@users.noreply.github.com>
2025-12-16 23:37:05 +0100

    Update owner-unbanchat.js

--- plugins/owner-unbanchat.js
+++ plugins/owner-unbanchat.js
@@ -3,20 +3,21 @@
 if (!global.tempCodes) global.tempCodes = {};
 
 let handler = async (m, { args }) => {
+
     if (!global.db.data.chats[m.chat].isBanned) {
         return m.reply('*✓ 𝐌𝐨𝐝𝐚𝐥𝐢𝐭𝐚̀ 𝐟𝐚𝐧𝐭𝐚𝐬𝐦𝐚 𝐠𝐢𝐚̀ 𝐝𝐢𝐬𝐚𝐭𝐭𝐢𝐯𝐚𝐭𝐚 ✓*');
     }
 
-    let code = Math.floor(1000 + Math.random() * 9000).toString();
-    global.tempCodes[m.chat] = code;
-
-    console.log(`🔐 Codice Reveal di questo bot: ${code}`);
-
-    setTimeout(() => {
-        if (global.tempCodes[m.chat] === code) delete global.tempCodes[m.chat];
-    }, 60_000);
-
     if (!args[0]) {
+        let code = Math.floor(1000 + Math.random() * 9000).toString();
+        global.tempCodes[m.chat] = code;
+
+        console.log(`🔐 Codice Reveal di questo bot: ${code}`);
+
+        setTimeout(() => {
+            if (global.tempCodes[m.chat] === code) delete global.tempCodes[m.chat];
+        }, 60_000);
+
         return m.reply(`𝐏𝐞𝐫 𝐝𝐢𝐬𝐚𝐭𝐭𝐢𝐯𝐚𝐫𝐞 𝐥𝐚 𝐌𝐨𝐝𝐚𝐥𝐢𝐭𝐚̀ 𝐟𝐚𝐧𝐭𝐚𝐬𝐦𝐚, 𝐢𝐧𝐬𝐞𝐫𝐢𝐬𝐜𝐢 𝐢𝐥 𝐜𝐨𝐝𝐢𝐜𝐞 𝐚 𝟒 𝐜𝐢𝐟𝐫𝐞. 𝐄𝐬: .𝐫𝐞𝐯𝐞𝐚𝐥 𝟏𝟐𝟑𝟒`);
     }
 
