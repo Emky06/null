b3a7884a0f6db41e2e3d4275ef905cd2d5be7c8b
AxtralWiZaRd <134928023+AxtralWiZaRd@users.noreply.github.com>
2025-12-16 23:32:51 +0100

    Update owner-unbanchat.js

--- plugins/owner-unbanchat.js
+++ plugins/owner-unbanchat.js
@@ -1,5 +1,4 @@
 // Plugin fatto da Axtral_WiZaRd
-let REVEAL_CODE = null;
 
 if (!global.tempCodes) global.tempCodes = {};
 
@@ -18,7 +17,7 @@ let handler = async (m, { args }) => {
     }, 60_000);
 
     if (!args[0]) {
-        return m.reply(`𝐏𝐞𝐫 𝐝𝐢𝐬𝐚𝐭𝐭𝐢𝐯𝐚𝐫𝐞 𝐥𝐚 𝐌𝐨𝐝𝐚𝐥𝐢𝐭𝐚̀ 𝐟𝐚𝐧𝐭𝐚𝐬𝐦𝐚, 𝐢𝐧𝐬𝐞𝐫𝐢𝐬𝐜𝐢 𝐢𝐥 𝐜𝐨𝐝𝐢𝐜𝐞 𝐚 𝟒 𝐜𝐢𝐟𝐫𝐞. 𝐄𝐬: .𝐫𝐞𝐯𝐞𝐚𝐥 ${code}`);
+        return m.reply(`𝐏𝐞𝐫 𝐝𝐢𝐬𝐚𝐭𝐭𝐢𝐯𝐚𝐫𝐞 𝐥𝐚 𝐌𝐨𝐝𝐚𝐥𝐢𝐭𝐚̀ 𝐟𝐚𝐧𝐭𝐚𝐬𝐦𝐚, 𝐢𝐧𝐬𝐞𝐫𝐢𝐬𝐜𝐢 𝐢𝐥 𝐜𝐨𝐝𝐢𝐜𝐞 𝐚 𝟒 𝐜𝐢𝐟𝐫𝐞. 𝐄𝐬: .𝐫𝐞𝐯𝐞𝐚𝐥 𝟏𝟐𝟑𝟒`);
     }
 
     if (args[0] !== global.tempCodes[m.chat]) {
