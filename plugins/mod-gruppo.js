91145d6856723435a9faf59b55d17ff9b941c96f
AxtralWiZaRd <134928023+AxtralWiZaRd@users.noreply.github.com>
2025-12-20 17:28:06 +0100

    Update mod-gruppo.js

--- plugins/mod-gruppo.js
+++ plugins/mod-gruppo.js
@@ -12,8 +12,9 @@ let handler = async (m, { conn }) => {
     const groupName = groupMetadata?.subject || 'Gruppo senza nome';
 
     let text = `╭━━━━━━━━━━━━━━━━━━━╮
-  _*𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐨𝐫𝐢 𝐝𝐢 ${groupName}*_ 
-╰━━━━━━━━━━━━━━━━━━━╯\n`;
+        _*𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐨𝐫𝐢 𝐝𝐢 
+  ${groupName}
+╰━━━━━━━━━━━━━━━━━━━╯\n╭━━━━━━━━━━━━━━━━━━━╮\n`;
 
     let mentions = [];
     prems.forEach(user => {
