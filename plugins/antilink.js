0ba87dedea677aa4e33698a6aaa56f4cd33f4d34
AxtralWiZaRd <134928023+AxtralWiZaRd@users.noreply.github.com>
2025-12-22 15:51:30 +0100

    Update antilink.js

--- plugins/antilink.js
+++ plugins/antilink.js
@@ -220,21 +220,21 @@ END:VCARD`
 async function handleKick({ conn, msg, sender, violation }) {
     await conn.groupSettingUpdate(msg.chat, 'announcement')
 
-    let messages = await conn.loadMessages(msg.chat, 100)
-    for (let m of messages) {
-        let text = extractText(m)
-        let cleanedText = normalizeText(text)
-        if (linkRegex.test(cleanedText) || channelRegex.test(cleanedText)) {
-            await conn.sendMessage(msg.chat, {
-                delete: {
-                    remoteJid: msg.chat,
-                    fromMe: false,
-                    id: m.key.id,
-                    participant: m.key.participant,
-                },
-            })
-        }
+    const messages = await conn.fetchMessages(msg.chat, { limit: 50 }) // ultimi 50 messaggi
+for (let m of messages) {
+    let text = extractText(m)
+    let cleanedText = normalizeText(text)
+    if (linkRegex.test(cleanedText) || channelRegex.test(cleanedText)) {
+        await conn.sendMessage(msg.chat, {
+            delete: {
+                remoteJid: msg.chat,
+                fromMe: false,
+                id: m.key.id,
+                participant: m.key.participant,
+            },
+        })
     }
+}
 
     await conn.sendMessage(msg.chat, {
         delete: {
