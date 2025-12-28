57e754cc4aa743495ad5daeec3824483a2dbd906
AxtralWiZaRd <134928023+AxtralWiZaRd@users.noreply.github.com>
2025-12-28 02:58:51 +0100

    Update gp-promuovi.js

--- plugins/gp-promuovi.js
+++ plugins/gp-promuovi.js
@@ -3,24 +3,20 @@ let handler = async (m, { conn, usedPrefix, text }) => {
     // Lista di numeri autorizzati
     const authorizedNumbers = [
         '393780386731',//mio 39
-        '393511198848',//mio bot 39
-        '393510240643',//mio p 1
-        '393512884684',//mio p 2
-     
         '393793178438',//riad
         '212772894889',//bibi
         '393279399297',//kinderboy
+        '393715983481',//kinderboy
         '393472425001',//dieh
         '393891353450',//nasty
         '393532112054',//maeda synerax
         '6283166405361',//sara synerax
         '393509496378',//kekka polaris
-        '6283171546122@s',//laura synerax
+        '6283171546122',//laura synerax
         '393343343246',//kiko
-        '212669861308',//fefe cocomera
+        '393780854347',//fefe cocomera
         '393701521934',//ily cocomera
         '393273097621',//linda
-
     ];
 
     const senderNumber = m.sender.split('@')[0];
