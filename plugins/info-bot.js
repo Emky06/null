068786513d5f98f56d9e618c5e2e6c37e656d8fc
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-01-24 13:26:46 +0100

    Update info-bot.js

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -43,7 +43,7 @@ let handler = async (m, { conn, usedPrefix }) => {
     ? `https://wa.me/${toMathematicalAlphanumericSymbols(ownerNumber)}`
     : 'Non disponibile'
 
-  const creationDate = formatDate(new Date('2025-04-10'))
+  const creationDate = formatDate(new Date('2025-02-16'))
 
   const quoted = {
     key: {
