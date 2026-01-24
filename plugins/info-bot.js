9d9ecd6eebd7ed7e4b4d6b157bac6017c3dfa4f6
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-01-24 13:22:32 +0100

    Update info-bot.js

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -4,7 +4,7 @@ import fs from 'fs'
 const toMathematicalAlphanumericSymbols = number => {
   const map = {
     '0': '𝟎', '1': '𝟏', '2': '𝟐', '3': '𝟑', '4': '𝟒',
-    '5': '𝟓', '6': '𝟔', '7': '𝟕', '8': '𝟖', '9': '𝟗'
+    '5': '𝟓', '6': '𝟔', '7': '𝟕', '8': '𝟖', '9': '𝟗', '10' : '𝟏𝟎'
   }
   return number.toString().split('').map(d => map[d] || d).join('')
 }
