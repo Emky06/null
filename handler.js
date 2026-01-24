18808373074ed8955c156f203b52b243a021e5c0
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-01-24 20:18:39 +0100

    Fix author comment in handler.js

--- handler.js
+++ handler.js
@@ -1,4 +1,4 @@
-//Fatto da Axtral_WiZaRd
+//Fatto da Axtral_WiZa
 import { generateWAMessageFromContent } from "@whiskeysockets/baileys"
 import { smsg } from './lib/simple.js'
 import { format } from 'util'
