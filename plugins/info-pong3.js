f16c25d16ef35651f471f3307bd69bb5620d3124
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-01-26 18:37:50 +0100

    Update info-pong3.js

--- plugins/info-pong3.js
+++ plugins/info-pong3.js
@@ -16,7 +16,7 @@ let handler = async (m, { conn, usedPrefix }) => {
 
     await conn.sendMessage(m.chat, {
       text: message,
-      footer: `𝐏𝐢𝐧𝐠 ${botName}`,
+      footer: `𝐏𝐢𝐧𝐠 ${nomebot}`,
       buttons: [
         { buttonId: `${usedPrefix}pong3`, buttonText: { displayText: "🔄 𝐏𝐨𝐧𝐠" }, type: 1 },
         { buttonId: `${usedPrefix}ds`, buttonText: { displayText: "🗑️ 𝐒𝐯𝐮𝐨𝐭𝐚 𝐬𝐞𝐬𝐬𝐢𝐨𝐧𝐢" }, type: 1 },
