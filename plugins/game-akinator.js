f25cf0d81402b88b1875024b004324edbdb3fbf8
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-01-26 04:19:00 +0100

    Update game-akinator.js

--- plugins/game-akinator.js
+++ plugins/game-akinator.js
@@ -1,5 +1,5 @@
-const { personaggiAnime } = require('./anime.js');
-const { personaggiGenerali } = require('./generale.js');
+import { personaggiAnime } from './akinator-anime.js';
+import { personaggiGenerali } from './akinator-generale.js';
 
 const activeGames = new Map();
 const pendingModeChoice = new Map();
