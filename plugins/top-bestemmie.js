759febc415f50ca79a9dc301ac6d2a34d25d64b1
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-01-27 00:04:55 +0100

    Update top-bestemmie.js

--- plugins/top-bestemmie.js
+++ plugins/top-bestemmie.js
@@ -1,55 +1,69 @@
+// Plugin Top 10 Bestemmiatori by Axtral_WiZaRd
+import fs from 'fs';
+
 let handler = async (m, { conn }) => {
-  if (!m.isGroup) return; // Funziona solo nei gruppi
+    const users = global.db.data.users || {};
 
-  // Ottieni i membri del gruppo
-  let groupMetadata = await conn.groupMetadata(m.chat);
-  let participants = groupMetadata.participants;
-
-  // Accedi al database degli utenti
-  let users = global.db.data.users;
-
-  // Assicurati che ogni partecipante abbia un record nel DB
-  participants.forEach(p => {
-    if (!users[p.id]) users[p.id] = { blasphemy: 0 };
-    if (typeof users[p.id].blasphemy !== 'number') users[p.id].blasphemy = 0;
-  });
-
-  // Filtra e ordina i membri con bestemmie
-  let groupUsers = participants
-    .map(p => ({
-      id: p.id,
-      bestemmie: users[p.id]?.blasphemy || 0
-    }))
-    .filter(u => u.bestemmie > 0)
-    .sort((a, b) => b.bestemmie - a.bestemmie)
-    .slice(0, 10); // Top 10
-
-  // Debug: stampiamo chi viene considerato
-  console.log('Top bestemmie:', groupUsers);
-
-  // Genera il messaggio della classifica
-  let text;
-  if (groupUsers.length === 0) {
-    text = "😇 Nessuno ha bestemmiato in questo gruppo!";
-  } else {
-    text = `🏆 *Top 10 Bestemmiatori del Gruppo* 🏆\n\n`;
-    groupUsers.forEach((user, index) => {
-      text += `${index + 1}. @${user.id.split('@')[0]} - ${user.bestemmie} bestemmie\n`;
+    // Assicuriamoci che tutti gli utenti abbiano il campo blasphemy
+    Object.keys(users).forEach(id => {
+        if (typeof users[id].blasphemy !== 'number') users[id].blasphemy = 0;
     });
-  }
 
-  // Invia il messaggio con menzioni
-  await conn.sendMessage(
-    m.chat,
-    { 
-      text, 
-      mentions: groupUsers.map(u => u.id) 
-    },
-    { quoted: m }
-  );
+    // Prendi i primi 10 con più bestemmie
+    let classifica = Object.entries(users)
+        .filter(([key, data]) => (data.blasphemy || 0) > 0)
+        .map(([key, data]) => ({ id: key, bestemmie: data.blasphemy }))
+        .sort((a, b) => b.bestemmie - a.bestemmie)
+        .slice(0, 10);
+
+    if (classifica.length === 0) {
+        return conn.reply(m.chat, "😇 Nessuno ha bestemmiato in questo gruppo!", m);
+    }
+
+    let message = `🏆 *Top 10 Bestemmiatori del Gruppo* 🏆\n\n`;
+    let mentions = [];
+    let userPosition = null;
+
+    classifica.forEach((user, index) => {
+        let medal = "🏅";
+        if (index === 0) medal = "🥇";
+        else if (index === 1) medal = "🥈";
+        else if (index === 2) medal = "🥉";
+
+        message += `${medal} *${index + 1}.* @${user.id.split('@')[0]} ➠ ${user.bestemmie} bestemmie\n`;
+        mentions.push(user.id);
+
+        if (user.id === m.sender) userPosition = index + 1;
+    });
+
+    let totalPlayers = Object.keys(users).length;
+    let userMessage = userPosition !== null
+        ? `𝐋𝐚 𝐭𝐮𝐚 𝐩𝐨𝐬𝐢𝐳𝐢𝐨𝐧𝐞 𝐞̀ ${userPosition}° 𝐬𝐮 ${totalPlayers}`
+        : `𝐋𝐚 𝐭𝐮𝐚 𝐩𝐨𝐬𝐢𝐳𝐢𝐨𝐧𝐞: 𝐧𝐞𝐬𝐬𝐮𝐧𝐚`;
+
+    const profileBuffer = fs.readFileSync('./icone/bestemmiometro.jpg'); // thumbnail
+
+    await conn.sendMessage(m.chat, {
+        text: message + `\n\n${userMessage}`,
+        mentions: mentions
+    }, {
+        quoted: {
+            key: {
+                participants: "0@s.whatsapp.net",
+                fromMe: false,
+                id: "Halo",
+            },
+            message: {
+                locationMessage: {
+                    name: "𝐁𝐞𝐬𝐭𝐞𝐦𝐦𝐢𝐨𝐦𝐞𝐭𝐫𝐨",
+                    jpegThumbnail: profileBuffer,
+                    vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
+                },
+            },
+            participant: "0@s.whatsapp.net",
+        },
+    });
 };
 
-// Registrazione del comando
-handler.command = ['topbestemmie', 'bestemmietop']; // Comandi accettati
-handler.group = true; // Solo nei gruppi
+handler.command = /^topbestemmie$/i;
 export default handler;
