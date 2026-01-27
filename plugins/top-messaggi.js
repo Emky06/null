aab59159e914a30f8acf3956020e8f50bb658c32
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-01-27 01:38:04 +0100

    Update top-messaggi.js

--- plugins/top-messaggi.js
+++ plugins/top-messaggi.js
@@ -36,16 +36,19 @@ let handler = async (m, { conn, args, participants }) => {
     let userPosition = null;
 
     usersData.forEach((user, i) => {
-        let medal = "🏅";
-        if (i === 0) medal = "🥇";
-        else if (i === 1) medal = "🥈";
-        else if (i === 2) medal = "🥉";
+    let medal = "🏅";
+    if (i === 0) medal = "🥇";
+    else if (i === 1) medal = "🥈";
+    else if (i === 2) medal = "🥉";
 
-        message += `${medal} *${i + 1}.* @${user.jid.split('@')[0]} ➠ ${user.messaggi} 𝐦𝐞𝐬𝐬𝐚𝐠𝐠𝐢\n`;
-        mentions.push(user.jid);
+    // Rimuove caratteri invisibili LEFT-TO-RIGHT ISOLATE ecc.
+    const cleanJid = user.jid.replace(/[\u2066-\u2069]/g, '');
 
-        if (user.jid === m.sender) userPosition = i + 1;
-    });
+    message += `${medal} *${i + 1}.* @${cleanJid} ➠ ${user.messaggi} messaggi\n`;
+    mentions.push(user.jid);
+
+    if (user.jid === m.sender) userPosition = i + 1;
+});
 
     let totalPlayers = participants.length;
     let userMessage = userPosition
