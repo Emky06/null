cd42cef4c79b67c876d280755652f775d312d28d
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-02-06 03:00:20 +0100

    Update antiinsta.js

--- plugins/antiinsta.js
+++ plugins/antiinsta.js
@@ -21,7 +21,7 @@ export async function before(m, { isAdmin, isPrems, groupMetadata, isBotAdmin, c
     if (!global.db.data.users[m.sender].warnReasons) global.db.data.users[m.sender].warnReasons = [];
 
     global.db.data.users[m.sender].warn += 1;
-    global.db.data.users[m.sender].warnReasons.push('link instagram');
+    global.db.data.users[m.sender].warnReasons.push('𝐋𝐢𝐧𝐤 𝐈𝐧𝐬𝐭𝐚𝐠𝐫𝐚𝐦');
 
     await conn.sendMessage(m.chat, {
       delete: {
