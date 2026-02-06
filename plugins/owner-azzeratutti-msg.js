947a37893725371ccf6e05fca0e191c7efda8d07
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-02-06 16:31:13 +0100

    Update owner-azzeratutti-msg.js

--- plugins/owner-azzeratutti-msg.js
+++ plugins/owner-azzeratutti-msg.js
@@ -8,12 +8,20 @@ const handler = async (m, { conn, participants, isOwner }) => {
         return conn.reply(m.chat, '❌ Solo il proprietario del bot può usare questo comando!', m);
     }
 
-    for (const p of participants) {
-        const user = global.db.data.users[p.id];
+    const ownerJids = global.owner.map(o => o[0] + '@s.whatsapp.net');
+
+    let usersToReset = participants
+        .map(p => p.jid)
+        .filter(jid => jid && !ownerJids.includes(jid));
+
+    for (const jid of usersToReset) {
+        const user = global.db.data.users[jid];
         if (user) {
             user.messaggi = 0;
         }
     }
+
+    if (global.db.write) await global.db.write();
 
     conn.reply(
         m.chat,
@@ -22,7 +30,7 @@ const handler = async (m, { conn, participants, isOwner }) => {
     );
 };
 
-handler.command = /^azzeratuttimsg$/i;
+handler.command = /^removeallmsg$/i;
 handler.rowner = true;
 handler.group = true;
 
