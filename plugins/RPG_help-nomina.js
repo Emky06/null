0da6e679ebc3c66142a0e9eca6b7661e36cd0fc3
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-02-06 04:20:24 +0100

    Update RPG_help-nomina.js

--- plugins/RPG_help-nomina.js
+++ plugins/RPG_help-nomina.js
@@ -4,16 +4,17 @@ const handlerNominaHelp = async (m, { conn }) => {
 
   const args = text.split(/\s+/);
   if (args.length === 1) {
-    // Solo .nomina senza argomenti: mostro la guida
+    
     return conn.reply(
       m.chat,
-      '✏️ Per rinominare un animale usa:\n.nomina [numero animale] [nuovo nome]\n\nEsempio:\n.nomina 1 Zeus',
+      '✏️ 𝐏𝐞𝐫 𝐫𝐢𝐧𝐨𝐦𝐢𝐧𝐚𝐫𝐞 𝐮𝐧 𝐚𝐧𝐢𝐦𝐚𝐥𝐞 𝐮𝐬𝐚:\n.nomina [numero animale] [nuovo nome]\n\nEsempio:\n.nomina 1 Zeus',
       m
     );
   }
-  // Se ci sono argomenti, passa il messaggio al plugin che gestisce la rinomina
+ 
 };
 
 handlerNominaHelp.command = /^helpnomina$/i;
-handlerNominaHelp.exp = 0;
+handler.group = true
+
 export default handlerNominaHelp;
