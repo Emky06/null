92094ae13e1c2ebe92a2ef007131f3c80384bfbd
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-02-07 18:28:20 +0100

    Update game-tris.js

--- plugins/game-tris.js
+++ plugins/game-tris.js
@@ -150,7 +150,11 @@ handler.before = async function(m) {
   if(!/^[1-9]$/.test(m.text)) return
 
   if(m.sender !== room.game.currentTurn) {
-    await m.reply(`❌ 𝐍𝐨𝐧 è 𝐢𝐥 𝐭𝐮𝐨 𝐭𝐮𝐫𝐧𝐨! 𝐓𝐨𝐜𝐜𝐚 𝐚 @${room.game.currentTurn.split('@')[0]}`)
+   
+    await this.sendMessage(m.chat, {
+      text: `❌ 𝐍𝐨𝐧 è 𝐢𝐥 𝐭𝐮𝐨 𝐭𝐮𝐫𝐧𝐨! 𝐓𝐨𝐜𝐜𝐚 𝐚 @${room.game.currentTurn}`,
+      mentions: [room.game.currentTurn]
+    }, { quoted: m })
     return
   }
 
@@ -172,6 +176,17 @@ handler.before = async function(m) {
       await m.reply('❌ 𝐂𝐞𝐥𝐥𝐚 𝐠𝐢à 𝐨𝐜𝐜𝐮𝐩𝐚𝐭𝐚')
       return
   }
+
+  if(room.game.winner) {
+    return finishGame(this, room, room.game.winner, false)
+  }
+  
+  if(room.game.board === 511) {
+    return finishGame(this, room, null, false)
+  }
+
+  return sendBoard(this, room, m)
+}
 
   if(room.game.winner) {
     return finishGame(this, room, room.game.winner, false)
