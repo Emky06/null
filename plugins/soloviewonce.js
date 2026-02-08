927d46f104b57338f6256ebce99a9a4dbc611f4b
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-02-08 17:47:36 +0100

    Update soloviewonce.js

--- plugins/soloviewonce.js
+++ plugins/soloviewonce.js
@@ -12,7 +12,7 @@ export async function before(m, { conn, isAdmin, isBotAdmin, isPrems }) {
     await conn.sendMessage(m.chat, {
       text: `> ⚠️𝐀𝐍𝐓𝐈𝐌𝐄𝐃𝐈𝐀 𝐀𝐓𝐓𝐈𝐕𝐎⚠️
 𝐋𝐚 𝐟𝐨𝐭𝐨/𝐯𝐢𝐝𝐞𝐨 𝐝𝐢 @${m.sender.split('@')[0]} 𝐞̀ 𝐬𝐭𝐚𝐭𝐚 𝐫𝐢𝐦𝐨𝐬𝐬𝐚.
-𝐒𝐨𝐧𝐨 𝐜𝐨𝐧𝐬𝐞𝐧𝐭𝐢𝐭𝐞 𝐬𝐨𝐥𝐨 𝐟𝐨𝐭𝐨 𝐞 𝐯𝐢𝐝𝐞𝐨 𝐚 1 𝐯𝐢𝐬𝐮𝐚𝐥①.`,
+𝐒𝐨𝐧𝐨 𝐜𝐨𝐧𝐬𝐞𝐧𝐭𝐢𝐭𝐞 𝐬𝐨𝐥𝐨 𝐟𝐨𝐭𝐨 𝐞 𝐯𝐢𝐝𝐞𝐨 𝐚𝐝 𝟏 𝐯𝐢𝐬𝐮𝐚𝐥①.`,
       mentions: [m.sender]
     });
 
