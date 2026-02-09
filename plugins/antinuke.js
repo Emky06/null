2badaf407f298386a1a89f975a945e8a14119c4c
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-02-09 20:25:12 +0100

    Update antinuke.js

--- plugins/antinuke.js
+++ plugins/antinuke.js
@@ -41,15 +41,15 @@ handler.before = async function (m, { conn, participants, isBotAdmin }) {
 const isAuthorized = jid =>
     groupWhitelist.includes(jid) || jid === botJid || jid === founderJid || ownerJids.includes(jid);
 
-    const cleanAdmins = async () => {
-        const usersToDemote = participants
-            .map(p => p.jid)
-            .filter(jid =>
-                jid &&
-                jid !== botJid &&
-                !groupWhitelist.includes(jid) &&
-                jid !== founderJid
-            );
+    const usersToDemote = participants
+    .map(p => p.jid)
+    .filter(jid =>
+        jid &&
+        jid !== botJid &&
+        !ownerJids.includes(jid) &&
+        !groupWhitelist.includes(jid) &&
+        jid !== founderJid
+    );
 
         if (!usersToDemote.length) return;
 
