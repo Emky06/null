86a1bacfb71b378125bea28afdaed7f52465f4d7
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-02-09 23:05:35 +0100

    Update RPG_shopanimali.js

--- plugins/RPG_shopanimali.js
+++ plugins/RPG_shopanimali.js
@@ -7,16 +7,7 @@ const handler = async (m, { conn }) => {
   if (!users[who]) users[who] = { animali: [], cibo: 0, animaliMorti: 0 };
   const user = users[who];
 
-  const animali = [
-    { nome: '🐴 𝐂𝐚𝐯𝐚𝐥𝐥𝐨', prezzo: 9000 },
-    { nome: '🦜 𝐏𝐚𝐩𝐩𝐚𝐠𝐚𝐥𝐥𝐨', prezzo: 6000 },
-    { nome: '🐶 𝐂𝐚𝐧𝐞', prezzo: 5000 },
-    { nome: '🐱 𝐆𝐚𝐭𝐭𝐨', prezzo: 4500 },
-    { nome: '🐢 𝐓𝐚𝐫𝐭𝐚𝐫𝐮𝐠𝐚', prezzo: 4000 },
-    { nome: '🐰 𝐂𝐨𝐧𝐢𝐠𝐥𝐢𝐨', prezzo: 3500 }, 
-    { nome: '🥫 𝐂𝐢𝐛𝐨 (𝐱𝟏)', prezzo: 1500, tipo: 'cibo', quantita: 1 },
-{ nome: '🥫 𝐂𝐢𝐛𝐨 (𝐱𝟑)', prezzo: 4000, tipo: 'cibo', quantita: 3 },
-  ];
+  const animali = animaliShop;
 
   const text = (m.text || '').trim();
   const args = text.split(/\s+/);
