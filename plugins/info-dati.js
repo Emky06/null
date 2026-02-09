76322c4cf29f6ff95002b8ab0c861482e39bdfc6
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-02-09 17:27:44 +0100

    Update info-dati.js

--- plugins/info-dati.js
+++ plugins/info-dati.js
@@ -1,7 +1,3 @@
-//Plugin fatto da Riad, mod by Axtral
-import fs from 'fs';
-import path from 'path';
-
 const handler = async (m, { conn }) => {
   try {
     const mention = m.mentionedJid?.[0] || (m.quoted ? m.quoted.sender : m.sender);
@@ -16,15 +12,12 @@ const handler = async (m, { conn }) => {
     const vittorieTris = user.vittorieTris || 0;
     const vittorieImpiccato = user.vittorieImpiccato || 0;
 
-    let pic;
+    let pp = './icone/profilo.png';
     try {
-      pic = await conn.profilePictureUrl(who, 'image');
-      
-      pic = await (await fetch(pic)).buffer();
-    } catch {
-      
-      pic = fs.readFileSync(path.join('./icone/profilo.png'));
-    }
+      pp = await conn.profilePictureUrl(who, 'image');
+    } catch {}
+
+    let apii = await conn.getFile(pp);
 
     let text = `
 𖦹━━━━━━ ☾︎•♦️•☽︎ ━━━━━━𖦹
@@ -48,7 +41,7 @@ const handler = async (m, { conn }) => {
         externalAdReply: {
           title: user.name || 'Utente sconosciuto',
           body: '𝑺𝒕𝒂𝒕𝒊𝒔𝒕𝒊𝒄𝒉𝒆 𝒅𝒆𝒊 𝒈𝒊𝒐𝒄𝒉𝒊 🕹️',
-          thumbnail: pic, 
+          thumbnail: apii.data
         }
       }
     }, { quoted: m });
