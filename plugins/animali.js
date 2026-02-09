a3f9a89da4d949882125bcad217bb3bb4de1568c
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-02-09 23:04:42 +0100

    Create animali.js

---
+++ plugins/animali.js
@@ -0,0 +1,14 @@
+export const animaliShop = [
+  { nome: '🐴 𝐂𝐚𝐯𝐚𝐥𝐥𝐨', prezzo: 9000 },
+  { nome: '🦜 𝐏𝐚𝐩𝐩𝐚𝐠𝐚𝐥𝐥𝐨', prezzo: 6000 },
+  { nome: '🐶 𝐂𝐚𝐧𝐞', prezzo: 5000 },
+  { nome: '🐱 𝐆𝐚𝐭𝐭𝐨', prezzo: 4500 },
+  { nome: '🐢 𝐓𝐚𝐫𝐭𝐚𝐫𝐮𝐠𝐚', prezzo: 4000 },
+  { nome: '🐰 𝐂𝐨𝐧𝐢𝐠𝐥𝐢𝐨', prezzo: 3500 }, 
+  { nome: '🥫 𝐂𝐢𝐛𝐨 (𝐱1)', prezzo: 1500, tipo: 'cibo', quantita: 1 },
+  { nome: '🥫 𝐂𝐢𝐛𝐨 (𝐱3)', prezzo: 4000, tipo: 'cibo', quantita: 3 },
+];
+
+export const animaliDisponibili = animaliShop
+  .filter(a => !a.tipo || a.tipo !== 'cibo')
+  .map(a => a.nome);
