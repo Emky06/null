09f02639ee1d693bb7cc3b6c7c9fc0ce6cdfc350
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-02-10 19:18:06 +0100

    Update tops.js

--- plugins/tops.js
+++ plugins/tops.js
@@ -1,5 +1,4 @@
-import util from 'util'
-import path from 'path'
+//Plugin fatto da Axtral_WiZaRd
 let user = a => '@' + a.split('@')[0]
 
 function shuffle(array) {
@@ -10,16 +9,24 @@ function shuffle(array) {
   return array
 }
 
-function handler(m, { groupMetadata, command, conn, participants }) {
-    let ps = groupMetadata.participants.map(v => v.id)
+function handler(m, { groupMetadata, command }) {
+  let ps = groupMetadata.participants.map(v => v.id)
 
-    if (ps.length < 10) {
-        return m.reply('Il gruppo deve avere almeno 10 partecipanti.')
+  if (ps.length < 10) {
+    return m.reply('Il gruppo deve avere almeno 10 partecipanti.')
+  }
+
+  if (['toplgbt','topnazi','topsexy','toptroie'].includes(command)) {
+    let [a,b,c,d,e,f,g,h,i,j] = shuffle([...ps]).slice(0, 10)
+
+    let titoli = {
+      toplgbt: '*🌈𝐓𝐎𝐏 𝟏𝟎 𝐋𝐆𝐁𝐓🌈*',
+      topnazi: '*࿖𝐓𝐎𝐏 𝟏𝟎 𝐍𝐀𝐙𝐈࿖*',
+      topsexy: '*😏𝐓𝐨𝐩 𝟏𝟎 𝐒𝐄𝐗𝐘😏*',
+      toptroie: '*𝐓𝐨𝐩 𝟏𝟎 𝐓𝐑𝐎𝐈𝐄*'
     }
 
-    if (command == 'topgays') {
-        let [a, b, c, d, e, f, g, h, i, j] = shuffle([...ps]).slice(0, 10)
-        let top = `*🌈𝐓𝐎𝐏 𝟏𝟎 𝐋𝐆𝐁𝐓🌈*
+    let top = `${titoli[command]}
 
 *1-➤* ${user(a)}
 *2-➤* ${user(b)}
@@ -31,62 +38,12 @@ function handler(m, { groupMetadata, command, conn, participants }) {
 *8-➤* ${user(h)}
 *9-➤* ${user(i)}
 *10-➤* ${user(j)}`
-        m.reply(top, null, { mentions: [a, b, c, d, e, f, g, h, i, j] })
-    }
 
-    if (command == 'topnazi') {
-        let [a, b, c, d, e, f, g, h, i, j] = shuffle([...ps]).slice(0, 10)
-        let top = `*࿖𝐓𝐎𝐏 𝟏𝟎 𝐍𝐀𝐙𝐈࿖*
-
-*1-➤* ${user(a)}
-*2-➤* ${user(b)}
-*3-➤* ${user(c)}
-*4-➤* ${user(d)}
-*5-➤* ${user(e)}
-*6-➤* ${user(f)}
-*7-➤* ${user(g)}
-*8-➤* ${user(h)}
-*9-➤* ${user(i)}
-*10-➤* ${user(j)}`
-        m.reply(top, null, { mentions: [a, b, c, d, e, f, g, h, i, j] })
-    }
-
-    if (command == 'topsexy') {
-        let [a, b, c, d, e, f, g, h, i, j] = shuffle([...ps]).slice(0, 10)
-        let top = `*😏𝐓𝐨𝐩 𝟏𝟎 𝐒𝐄𝐗𝐘😏*
-
-*1-➤* ${user(a)}
-*2-➤* ${user(b)}
-*3-➤* ${user(c)}
-*4-➤* ${user(d)}
-*5-➤* ${user(e)}
-*6-➤* ${user(f)}
-*7-➤* ${user(g)}
-*8-➤* ${user(h)}
-*9-➤* ${user(i)}
-*10-➤* ${user(j)}`
-        m.reply(top, null, { mentions: [a, b, c, d, e, f, g, h, i, j] })
-    }
-
-    if (command == 'toptroie') {
-        let [a, b, c, d, e, f, g, h, i, j] = shuffle([...ps]).slice(0, 10)
-        let top = `*𝐓𝐨𝐩 𝟏𝟎 𝐓𝐑𝐎𝐈𝐄*
-
-*1-➤* ${user(a)}
-*2-➤* ${user(b)}
-*3-➤* ${user(c)}
-*4-➤* ${user(d)}
-*5-➤* ${user(e)}
-*6-➤* ${user(f)}
-*7-➤* ${user(g)}
-*8-➤* ${user(h)}
-*9-➤* ${user(i)}
-*10-➤* ${user(j)}`
-        m.reply(top, null, { mentions: [a, b, c, d, e, f, g, h, i, j] })
-    }
+    m.reply(top, null, { mentions: [a,b,c,d,e,f,g,h,i,j] })
+  }
 }
 
-handler.help = handler.command = ['topgays','topnazi', 'topsexy','toptroie']
+handler.help = handler.command = ['toplgbt','topnazi', 'topsexy','toptroie']
 handler.tags = ['games']
 handler.group = true
 handler.admin = true
