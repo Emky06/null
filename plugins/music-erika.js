d1a9832a2e9b2d36429f6e672fe7035120904165
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-02-12 01:40:02 +0100

    Update music-erika.js

--- plugins/music-erika.js
+++ plugins/music-erika.js
@@ -1,10 +1,10 @@
 //Plugin fatto da Axtral_WiZaRd
 let handler = async (m, { conn }) => {
   try {
-    // Percorso del file musicale
+    
     let audioPath = './storage/mp3/erika.mp3';
 
-    // Invia il file audio come messaggio nel gruppo
+    
     await conn.sendMessage(m.chat, { 
   audio: { url: audioPath }, 
   mimetype: 'audio/mpeg' 
@@ -15,9 +15,9 @@ let handler = async (m, { conn }) => {
   }
 };
 
-handler.command = /^(erika)$/i;  // Comando 
-handler.group = true;  // Funziona solo nei gruppi
-handler.admin = true;  // Solo per amministratori
-handler.botAdmin = true;  // Il bot deve essere admin nel gruppo
+handler.command = /^(erika)$/i;  
+handler.group = true;  
+handler.premium = true;  
+handler.botAdmin = true;  
 
 export default handler;
