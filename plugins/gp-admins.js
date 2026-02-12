f8b90a0642e80210785e0234b671bb2a28b60827
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-02-12 02:07:00 +0100

    Update gp-admins.js

--- plugins/gp-admins.js
+++ plugins/gp-admins.js
@@ -1,41 +1,45 @@
-// Plugin fatto da Axtral_WiZaRd
-const handler = async (m, { conn, participants, groupMetadata, args }) => {
-        const botId = conn.user.id.split(':')[0] + '@s.whatsapp.net';
+//Plugin fatto da Axtral_WiZaRd
+let handler = async (m, { conn, participants, groupMetadata, args }) => {
+    const botId = conn.user.id.split(':')[0] + '@s.whatsapp.net';
 
-    const groupAdmins = participants.filter(p => p.admin && p.id !== botId);
-    const listAdmin = groupAdmins
-        .map((v, i) => `✧👑 ${i + 1}. @${v.id.split('@')[0]}`)
-        .join('\n');
+    const groupAdmins = participants
+        .filter(p => p.admin && p.jid !== botId)
+        .map(p => p.jid);
 
-    const owner = groupMetadata.owner || 
-        groupAdmins.find(p => p.admin === 'superadmin')?.id || 
-        `${m.chat.split`-`[0]}@s.whatsapp.net`;
+    const groupName = groupMetadata?.subject || 'Gruppo senza nome';
 
     let pesan = args.join(' ');
     let message = pesan ? pesan : '❌ Nessun messaggio fornito';
 
-    let text = `
-╭═══════════════════╮
-│    ⚠️ 𝐒𝐕𝐄𝐆𝐋𝐈𝐀 𝐀𝐃𝐌𝐈𝐍 ⚠️    │
-╰═══════════════════╯
+    const num = ['𝟏','𝟐','𝟑','𝟒','𝟓','𝟔','𝟕','𝟖','𝟗','𝟎'];
 
-✎ *Messaggio:*  
+    let text = `╭━━━━━━━━━━━━━━━━━━━╮
+         _*𝐀𝐝𝐦𝐢𝐧 𝐝𝐢  ${groupName}*_
+╰━━━━━━━━━━━━━━━━━━━╯
+
+✎ *𝐌𝐞𝐬𝐬𝐚𝐠𝐠𝐢𝐨:*  
 ➥ ${message}
 
-♔ *Lista Admin:*  
-${listAdmin}
+╭━━━━━━━━━━━━━━━━━━━╮
+`;
 
-╭═══════════════════╮
-│      ☄️ 𝔸𝕩𝕥𝕣𝕒𝕝_𝕎𝕚ℤ𝕒ℝ𝕕 ☄️     │
-╰═══════════════════╯
-`.trim();
+    let mentions = [];
 
-    conn.reply(m.chat, text, m, { mentions: [...groupAdmins.map(v => v.id), owner] });
+    if (groupAdmins.length) {
+        groupAdmins.forEach((jid, i) => {
+            mentions.push(jid);
+            text += `┣➤ 🛡️ ${num[i] || i + 1} @${jid.split('@')[0]}\n`;
+        });
+    }
+
+    text += `╰━━━━━━━━━━━━━━━━━━━╯`;
+
+    await conn.sendMessage(m.chat, { text, mentions });
 };
 
-handler.command = ['admins', '@admins', 'dmins'];
-handler.tags = ['group'];
 handler.help = ['admins <messaggio>'];
+handler.tags = ['group'];
+handler.command = ['admins', '@admins', 'dmins'];
 handler.group = true;
 
 export default handler;
