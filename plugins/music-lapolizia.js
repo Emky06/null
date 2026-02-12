3fb332bed9a572bfeee1f2a055d4b94425063f8a
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-02-12 06:09:53 +0100

    Update music-lapolizia.js

--- plugins/music-lapolizia.js
+++ plugins/music-lapolizia.js
@@ -17,7 +17,7 @@ let handler = async (m, { conn }) => {
 
 handler.command = /^(polizia)$/i;  
 handler.group = true;  
-handler.premium = true;  
+handler.staff = true;
 handler.botAdmin = true;  
 
 export default handler;
