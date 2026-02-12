688192392c8288a455783aca40b3f0d17393d872
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-02-12 06:31:19 +0100

    Update ai-shazam.js

--- plugins/ai-shazam.js
+++ plugins/ai-shazam.js
@@ -1,5 +1,6 @@
 import fs from 'fs'
 import acrcloud from 'acrcloud'
+
 let acr = new acrcloud({
   host: 'identify-eu-west-1.acrcloud.com',
   access_key: 'c33c767d683f78bd17d4bd4991955d81',
@@ -9,34 +10,42 @@ let acr = new acrcloud({
 let handler = async (m) => {
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
+
   if (/audio|video/.test(mime)) {
-   // if ((q.msg || q).seconds > 20) return m.reply('╰⊱⚠️⊱ *ATTENZIONE | WARNING* ⊱⚠️⊱╮\n\nIl file che hai caricato è troppo grande, ti consigliamo di tagliare il file in un frammento più piccolo. 10-20 secondi di audio sono sufficienti per l\'identificazione')
+
     await conn.reply(m.chat, wait, m)
+
     let media = await q.download()
     let ext = mime.split('/')[1]
-    fs.writeFileSync(`./tmp/${m.sender}.${ext}`, media)
-    let res = await acr.identify(fs.readFileSync(`./tmp/${m.sender}.${ext}`))
+    let filePath = `./tmp/${m.sender}.${ext}`
+
+    fs.writeFileSync(filePath, media)
+
+    let res = await acr.identify(fs.readFileSync(filePath))
     let { code, msg } = res.status
     if (code !== 0) throw msg
+
     let { title, artists, album, genres, release_date } = res.metadata.music[0]
+
     let txt = `
 RISULTATO DELLA RICERCA
 
 • 📌 TITOLO: ${title}
-• 👨‍🎤 ARTISTA: ${artists !== undefined ? artists.map(v => v.name).join(', ') : 'Non trovato'}
-• 💾 ALBUM: ${album.name || 'Non trovato'}
-• 🌐 GENERE: ${genres !== undefined ? genres.map(v => v.name).join(', ') : 'Non trovato'}
+• 👨‍🎤 ARTISTA: ${artists ? artists.map(v => v.name).join(', ') : 'Non trovato'}
+• 💾 ALBUM: ${album?.name || 'Non trovato'}
+• 🌐 GENERE: ${genres ? genres.map(v => v.name).join(', ') : 'Non trovato'}
 • 📆 DATA DI PUBBLICAZIONE: ${release_date || 'Non trovato'}
 `.trim()
-    fs.unlinkSync(`./tmp/${m.sender}.${ext}`)
 
-    const messageOptions = {}; 
-      }
-    };
+    fs.unlinkSync(filePath)
 
-    
-    m.reply(txt, null, { ...messageOptions })
-  } else throw '╰⊱❗️⊱ *USO ERRATO* ⊱❗️⊱╮\n\nRISPONDI A UN AUDIO O VIDEO'
+    const messageOptions = {}
+    m.reply(txt, null, messageOptions)
+
+  } else {
+    throw '╰⊱❗️⊱ *USO ERRATO* ⊱❗️⊱╮\n\nRISPONDI A UN AUDIO O VIDEO'
+  }
 }
+
 handler.command = /^shazam$/i
 export default handler
