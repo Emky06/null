477b8c1ecf89bf84d97cf8ddd88882c74fec719d
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-02-12 06:08:56 +0100

    Update music-erika.js

--- plugins/music-erika.js
+++ plugins/music-erika.js
@@ -17,7 +17,7 @@ let handler = async (m, { conn }) => {
 
 handler.command = /^(erika)$/i;  
 handler.group = true;  
-handler.premium = true;  
+handler.staff = true;
 handler.botAdmin = true;  
 
 export default handler;
