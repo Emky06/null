41b92d08f966d98b6b8bc037de4529b14657c5db
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-02-24 21:21:24 +0100

    Update owner-autoadmin.js

--- plugins/owner-autoadmin.js
+++ plugins/owner-autoadmin.js
@@ -1,11 +1,6 @@
 //Plugin creato da Axtral_WiZaRd
-let handler = async (m, { conn, isAdmin }) => {  
-    if (isAdmin) {
-        await conn.sendMessage(m.chat, { text: '𝑺𝒕𝒂𝒊 𝒈𝒊𝒂̀ 𝒓𝒆𝒈𝒏𝒂𝒏𝒅𝒐 𝒊𝒏 𝒒𝒖𝒆𝒔𝒕𝒐 𝒈𝒓𝒖𝒑𝒑𝒐!' });
-        return;
-    }
-
-    try {  
+let handler = async (m) => {  
+       try {  
         await conn.groupParticipantsUpdate(m.chat, [m.sender], "promote");
         await conn.sendMessage(m.chat, { text: '𝑯𝒂𝒊 𝒓𝒆𝒄𝒍𝒂𝒎𝒂𝒕𝒐 𝒊𝒍 𝒕𝒓𝒐𝒏𝒐. 𝑶𝒓𝒂 𝒓𝒆𝒈𝒏𝒊 𝒔𝒖 𝒒𝒖𝒆𝒔𝒕𝒐 𝒈𝒓𝒖𝒑𝒑𝒐.' });
     } catch (error) {
