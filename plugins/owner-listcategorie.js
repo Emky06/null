20c630e1f5d46fedfefaa16595f77b40149217b8
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-03-01 23:56:43 +0100

    Update owner-listcategorie.js

--- plugins/owner-listcategorie.js
+++ plugins/owner-listcategorie.js
@@ -1,5 +1,5 @@
 //Plugin fatto da Axtral_WiZaRd
-import { ROLE_IMPORTANCE } from "./lista-categorie.js";
+import { ROLE_IMPORTANCE } from "./lista_categorie.js";
 
 const handler = async (m, { conn }) => {
   if (!m.isGroup)
