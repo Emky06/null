7acff871aacddcbfda6a0060d7dc630e970c678a
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-03-02 00:22:42 +0100

    Update owner-elimina-categoria.js

--- plugins/owner-elimina-categoria.js
+++ plugins/owner-elimina-categoria.js
@@ -2,7 +2,7 @@ const handler = async (m, { conn, args }) => {
   const mention = m.mentionedJid?.[0] || m.quoted?.sender;
 
   if (!mention) {
-    return conn.sendMessage(m.chat, { text: `❌ Usa: .removecategoria @utente` }, { quoted: m });
+    return conn.sendMessage(m.chat, { text: `❌ 𝐔𝐬𝐚: .delcategoria @utente` }, { quoted: m });
   }
 
   if (!global.db.data.users[mention]) {
@@ -10,14 +10,14 @@ const handler = async (m, { conn, args }) => {
   }
 
   if (!global.db.data.users[mention].categoria) {
-    return conn.sendMessage(m.chat, { text: `⚠️ L'utente non ha nessuna categoria impostata.` }, { quoted: m });
+    return conn.sendMessage(m.chat, { text: `⚠️ 𝐋'𝐮𝐭𝐞𝐧𝐭𝐞 𝐧𝐨𝐧 𝐡𝐚 𝐧𝐞𝐬𝐬𝐮𝐧𝐚 𝐜𝐚𝐭𝐞𝐠𝐨𝐫𝐢𝐚 𝐢𝐦𝐩𝐨𝐬𝐭𝐚𝐭𝐚.` }, { quoted: m });
   }
 
   delete global.db.data.users[mention].categoria;
 
-  await conn.sendMessage(m.chat, { text: `✅ Categoria di @${mention.split("@")[0]} rimossa con successo.`, mentions: [mention] }, { quoted: m });
+  await conn.sendMessage(m.chat, { text: `✅ 𝐂𝐚𝐭𝐞𝐠𝐨𝐫𝐢𝐚 𝐝𝐢 @${mention.split("@")[0]} rimossa 𝐜𝐨𝐧 𝐬𝐮𝐜𝐜𝐞𝐬𝐬𝐨.`, mentions: [mention] }, { quoted: m });
 };
 
 handler.command = /^(eliminacategoria|delcategoria)$/i;
-handler.owner = true;  // solo owner può usarlo
+handler.owner = true; 
 export default handler;
