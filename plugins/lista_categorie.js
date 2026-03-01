2929e26730fbb1abe5bda870b1bbde983a3cdf57
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-03-02 00:15:01 +0100

    Update lista_categorie.js

--- plugins/lista_categorie.js
+++ plugins/lista_categorie.js
@@ -1,76 +1,11 @@
 //Plugin fatto da Axtral_WiZaRd
-export const CATEGORIE = [
-  { 
-    display: "𝐂𝐨𝐥𝐥𝐚𝐛𝐨𝐫𝐚𝐭𝐨𝐫𝐞 🤝🏻",
-    variants: ["collaboratore"]
-  },
-  { 
-    display: "𝐕𝐢𝐩 💎",
-    variants: ["vip"]
-  },
-  { 
-    display: "𝐕𝐞𝐭𝐞𝐫𝐚𝐧𝐨/𝐚 🎖️",
-    variants: ["veterano", "veterana"]
-  },
-  { 
-    display: "𝐋𝐮𝐝𝐨𝐩𝐚𝐭𝐢𝐜𝐨/𝐚 🎰",
-    variants: ["ludopatico", "ludopatica"]
-  },
-  { 
-    display: "𝐂𝐚𝐠𝐚𝐜𝐚𝐳𝐳𝐨 🙄",
-    variants: ["cagacazzo"]
-  },
-  { 
-    display: "𝐃𝐢𝐬𝐚𝐛𝐢𝐥𝐞 ♿",
-    variants: ["disabile"]
-  },
-  { 
-    display: "𝐌𝐨𝐫𝐭𝐨 𝐝𝐢 𝐟𝐢𝐠𝐚 🤤",
-    variants: ["morto di figa"]
-  },
-  { 
-    display: "𝐏𝐢𝐜𝐤 𝐦𝐞 💅🏻",
-    variants: ["pick me"]
-  },
-  { 
-    display: "𝐁𝐞𝐬𝐭𝐞𝐦𝐦𝐢𝐚𝐭𝐨𝐫𝐞/𝐭𝐫𝐢𝐜𝐞 🤬",
-    variants: ["bestemmiatore", "bestemmiatrice"]
-  },
-  { 
-    display: "𝐓𝐫𝐨𝐢𝐚 🥵",
-    variants: ["troia"]
-  },
-  { 
-    display: "𝐏𝐮𝐭𝐭𝐚𝐧𝐢𝐞𝐫𝐞 😎",
-    variants: ["puttaniere"]
-  },
-  { 
-    display: "𝐍𝐞𝐫𝐝 🤓",
-    variants: ["nerd"]
-  },
-];
 
-
-export const ROLE_IMPORTANCE = (() => {
-  let importance = {};
-  let total = CATEGORIE.length;
-
-  CATEGORIE.forEach((cat, index) => {
-    const value = total - index;
-    cat.variants.forEach(v => {
-      importance[v] = value;
-    });
-  });
-
-  return importance;
-})();
-
+import { CATEGORIE } from "./categorie.js";
 
 const handler = async (m, { conn }) => {
 
-  const testo = `📂 *𝐂𝐚𝐭𝐞𝐠𝐨𝐫𝐢𝐞 𝐝𝐢𝐬𝐩𝐨𝐧𝐢𝐛𝐢𝐥𝐢:*\n\n${CATEGORIE
-    .map((c, i) => `${i + 1}. ${c.display}`)
-    .join("\n")}`;
+  const testo = `📂 *𝐂𝐚𝐭𝐞𝐠𝐨𝐫𝐢𝐞 𝐝𝐢𝐬𝐩𝐨𝐧𝐢𝐛𝐢𝐥𝐢:*\n\n` +
+    CATEGORIE.map((c, i) => `${i + 1}. ${c.display}`).join("\n");
 
   await conn.sendMessage(
     m.chat,
@@ -80,6 +15,6 @@ const handler = async (m, { conn }) => {
 };
 
 handler.command = /^categorie$/i;
-handler.group = true
+handler.group = true;
 
 export default handler;
