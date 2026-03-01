5143211911ee4bf3f05ee42b1d66c691bf6e83d3
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-03-01 23:52:25 +0100

    Update lista_categorie.js

--- plugins/lista_categorie.js
+++ plugins/lista_categorie.js
@@ -1,44 +1,84 @@
+//Plugin fatto da Axtral_WiZaRd
+export const CATEGORIE = [
+  { 
+    display: "𝐂𝐨𝐥𝐥𝐚𝐛𝐨𝐫𝐚𝐭𝐨𝐫𝐞 🤝🏻",
+    variants: ["collaboratore"]
+  },
+  { 
+    display: "𝐕𝐢𝐩 💎",
+    variants: ["vip"]
+  },
+  { 
+    display: "𝐕𝐞𝐭𝐞𝐫𝐚𝐧𝐨/𝐚 🎖️",
+    variants: ["veterano", "veterana"]
+  },
+  { 
+    display: "𝐋𝐮𝐝𝐨𝐩𝐚𝐭𝐢𝐜𝐨/𝐚 🎰",
+    variants: ["ludopatico", "ludopatica"]
+  },
+  { 
+    display: "𝐂𝐚𝐠𝐚𝐜𝐚𝐳𝐳𝐨 🙄",
+    variants: ["cagacazzo"]
+  },
+  { 
+    display: "𝐃𝐢𝐬𝐚𝐛𝐢𝐥𝐞 ♿",
+    variants: ["disabile"]
+  },
+  { 
+    display: "𝐌𝐨𝐫𝐭𝐨 𝐝𝐢 𝐟𝐢𝐠𝐚 🤤",
+    variants: ["morto di figa"]
+  },
+  { 
+    display: "𝐏𝐢𝐜𝐤 𝐦𝐞 💅🏻",
+    variants: ["pick me"]
+  },
+  { 
+    display: "𝐁𝐞𝐬𝐭𝐞𝐦𝐦𝐢𝐚𝐭𝐨𝐫𝐞/𝐭𝐫𝐢𝐜𝐞 🤬",
+    variants: ["bestemmiatore", "bestemmiatrice"]
+  },
+  { 
+    display: "𝐓𝐫𝐨𝐢𝐚 🥵",
+    variants: ["troia"]
+  },
+  { 
+    display: "Puttaniere 😎",
+    variants: ["puttaniere"]
+  },
+  { 
+    display: "Nerd 🤓",
+    variants: ["nerd"]
+  },
+];
+
+
+export const ROLE_IMPORTANCE = (() => {
+  let importance = {};
+  let total = CATEGORIE.length;
+
+  CATEGORIE.forEach((cat, index) => {
+    const value = total - index;
+    cat.variants.forEach(v => {
+      importance[v] = value;
+    });
+  });
+
+  return importance;
+})();
+
+
 const handler = async (m, { conn }) => {
-  if (!m.isGroup) {
-    return conn.sendMessage(m.chat, { text: "❌ Questo comando può essere usato solo nei gruppi." }, { quoted: m });
-  }
 
-  const groupMetadata = await conn.groupMetadata(m.chat);
-  const participants = groupMetadata.participants;
-  const senderId = m.sender;
+  const testo = `📂 *Categorie disponibili:*\n\n${CATEGORIE
+    .map((c, i) => `${i + 1}. ${c.display}`)
+    .join("\n")}`;
 
-  const senderParticipant = participants.find(p => p.id === senderId);
-  if (!senderParticipant) return;
-
-  const isAdminOrHigher =
-    senderParticipant.admin === "admin" ||
-    senderParticipant.admin === "superadmin" ||
-    senderId === groupMetadata.owner;
-
-  if (!isAdminOrHigher) {
-    return conn.sendMessage(m.chat, {
-      text: "❌ Solo admin, co-owner o owner possono usare questo comando."
-    }, { quoted: m });
-  }
-
-  const categorie = [
-    "𝐎𝐰𝐧𝐞𝐫 👑",
-    "𝐂𝐨-𝐨𝐰𝐧𝐞𝐫 ⚔️",
-    "𝐂𝐨𝐥𝐥𝐚𝐛𝐨𝐫𝐚𝐭𝐨𝐫𝐞 🤝🏻",
-    "𝐕𝐢𝐩 💎",
-    "𝐕𝐞𝐭𝐞𝐫𝐚𝐧𝐨/𝐚 🎖️",
-    "𝐋𝐮𝐝𝐨𝐩𝐚𝐭𝐢𝐜𝐨/𝐚 🎰",
-    "𝐂𝐚𝐠𝐚𝐜𝐚𝐳𝐳𝐨 🙄",
-    "𝐃𝐢𝐬𝐚𝐛𝐢𝐥𝐞 ♿",
-    "𝐌𝐨𝐫𝐭𝐨 𝐝𝐢 𝐟𝐢𝐠𝐚 🤤",
-    "𝐏𝐢𝐜𝐤 𝐦𝐞 💅🏻",
-    "𝐁𝐨𝐭 🤖"
-  ];
-
-  const testo = `📂 *Categorie disponibili:*\n\n${categorie.map((c, i) => `${i + 1}. ${c}`).join("\n")}`;
-
-  await conn.sendMessage(m.chat, { text: testo }, { quoted: m });
+  await conn.sendMessage(
+    m.chat,
+    { text: testo },
+    { quoted: m }
+  );
 };
 
 handler.command = /^categorie$/i;
+
 export default handler;
