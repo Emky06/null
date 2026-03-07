8e69d715fea5334325fb3d7cebb4a4ab1b5a3521
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-03-07 16:10:33 +0100

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ global.confirmCode = ""
 
 global.owner = [
     ['19098183891', 'Owner', true],
-    ['13802054551', 'Owner', true],
+    ['212617142364', 'Owner', true],
     ['14455444258', 'Vero voip', true],
     ['393511198848', 'Owner', true],
     ['393510240643', 'Owner', true],
