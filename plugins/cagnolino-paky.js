c145db62a46003f8a403264a87acb423a8202fa4
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-03-20 00:44:14 +0100

    Update cagnolino-paky.js

--- plugins/cagnolino-paky.js
+++ plugins/cagnolino-paky.js
@@ -1,11 +1,11 @@
 // Plugin manda una foto a visual
 const handler = async (m, { conn }) => {
   await conn.sendMessage(m.chat, {
-    image: { url: './storage/image/cagnolino.jpg' }, // Assicurati che il file esista nella root del bot
+    image: { url: './storage/image/cagnolino.jpg' }, 
     viewOnce: true,
     caption: '*𝑬𝒄𝒄𝒐 𝒊𝒍 𝒄𝒖𝒄𝒄𝒊𝒐𝒍𝒐 𝒅𝒊 𝑨𝒙𝒕𝒓𝒂𝒍 𝒆 𝑲𝒊𝒏𝒅𝒆𝒓𝒊𝒏𝒐 🥰*'
   }, { quoted: m });
 };
 
-handler.command = /^cagnolino$/i;
+handler.command = /^jaise9i2wijci$/i;
 export default handler;
