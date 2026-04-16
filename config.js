54186a3905b606733cade7576c7d10d407b1103a
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-04-16 03:26:36 +0200

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ global.confirmCode = ""
 global.owner = [
     ['17577575541', 'Owner', true],
     ['212617142364', 'Owner', true],
-    ['39351288468', 'Owner', true],
+    ['393512884684', 'Owner', true],
     ['393511198848', 'Owner', true],
     ['393780386731', 'Owner', true],
     ['393510240643', 'Owner', true],
