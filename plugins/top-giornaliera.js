80718618e7f72a2e50a61d46853356622fcd5f6d
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-04-18 20:30:41 +0200

    Update top-giornaliera.js

--- plugins/top-giornaliera.js
+++ plugins/top-giornaliera.js
@@ -51,19 +51,12 @@ export async function dailyTopMessageCounter(m, { conn }) {
 
   const db = ensureDailyReset();
   if (!global.processedDailyTopMessages) global.processedDailyTopMessages = new Set();
-
-  const msgId = m?.key?.id;
-  if (!msgId) return;
-
-  if (global.processedDailyTopMessages.has(msgId)) return;
-  global.processedDailyTopMessages.add(msgId);
+  if (global.processedDailyTopMessages.has(m.key.id)) return;
+  global.processedDailyTopMessages.add(m.key.id);
 
   if (!db.dailyTop[m.chat]) db.dailyTop[m.chat] = { utenti: {} };
   const chat = db.dailyTop[m.chat];
-
-  if (!chat.utenti) chat.utenti = {};
   if (!chat.utenti[m.sender]) chat.utenti[m.sender] = { messaggi: 0 };
-
   chat.utenti[m.sender].messaggi++;
 
   saveDB(db);
