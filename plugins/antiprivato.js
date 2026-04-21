f593a3360d7ffd8e266a3cfc26d50f48408216d7
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-04-22 00:19:38 +0200

    Update antiprivato.js

--- plugins/antiprivato.js
+++ plugins/antiprivato.js
@@ -1,21 +1,17 @@
 export async function before(m, { conn, isOwner, isROwner }) {
-  // Ignora messaggi del bot stesso
   if (m.isBaileys && m.fromMe) return true;
-
-  // Ignora i gruppi
   if (m.isGroup) return false;
-
-  // Ignora se il messaggio è vuoto
   if (!m.message) return true;
 
-  // Recupera impostazioni del bot
   const settings = global.db.data.settings[conn.user.jid] || {};
 
-  // Se antiprivato è attivo e l'utente non è owner o real owner
   if (settings.antiprivato && !isOwner && !isROwner) {
- 
-    // Blocca l'utente
-    await conn.updateBlockStatus(m.sender, 'block');
+
+    const user = m.sender?.split(':')[0];
+
+    if (user && user.endsWith('@s.whatsapp.net')) {
+      await conn.updateBlockStatus(user, 'block');
+    }
   }
 
   return false;
