d6b244fa2062cd8abeb69fd3dd3d1fd948847bf3
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-04-22 20:10:50 +0200

    Update antiprivato.js

--- plugins/antiprivato.js
+++ plugins/antiprivato.js
@@ -1,14 +1,3 @@
-function cleanJid(jid) {
-  if (!jid) return null;
-  jid = jid.split(':')[0];
-
-  if (!jid.includes('@')) {
-    jid = jid.replace(/\D/g, '') + '@s.whatsapp.net';
-  }
-
-  return jid;
-}
-
 export async function before(m, { conn, isOwner, isROwner }) {
   if (m.isBaileys && m.fromMe) return true;
   if (m.isGroup) return false;
@@ -17,21 +6,8 @@ export async function before(m, { conn, isOwner, isROwner }) {
   const settings = global.db.data.settings[conn.user.jid] || {};
 
   if (settings.antiprivato && !isOwner && !isROwner) {
-    try {
-      let jid = cleanJid(m.key.remoteJid);
 
-      if (!jid || jid === 'status@broadcast') return;
-      if (!jid.endsWith('@s.whatsapp.net')) return;
-
-      console.log('BLOCK DEBUG:', jid);
-
-      await conn.reportAndBlockUser(jid);
-
-      console.log('UTENTE BLOCCATO (REPORT)');
-
-    } catch (e) {
-      console.error('Errore block:', e?.output?.statusCode, e?.message);
-    }
+    await conn.updateBlockStatus(m.chat, 'block');
   }
 
   return false;
