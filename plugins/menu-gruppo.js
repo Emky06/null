bdf32422b49f85cae251d4a883babf42c2809233
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-04-25 00:51:50 +0200

    Update menu-gruppo.js

--- plugins/menu-gruppo.js
+++ plugins/menu-gruppo.js
@@ -139,6 +139,8 @@ function generateMenuText(prefix) {
 ┃🔄 ${prefix}𝐫𝐞𝐯𝐞𝐫𝐬𝐞𝐯𝐢𝐝
 ┃⏩ ${prefix}𝐬𝐩𝐞𝐞𝐝𝐯𝐢𝐝𝐞𝐨
 ┃🎶 ${prefix}𝐩𝐥𝐚𝐲  
+┃🎥 ${prefix}𝐝𝐥𝐝𝐭𝐢𝐤𝐭𝐨𝐤
+┃🎥 ${prefix}𝐝𝐥𝐝𝐢𝐠
 ┃ⓘ  ${prefix}𝐢𝐧𝐟𝐨  
 ┃🌦️ ${prefix}𝐦𝐞𝐭𝐞𝐨 (𝐜𝐢𝐭𝐭𝐚̀)  
 ┃🎥 ${prefix}𝐭𝐨𝐯𝐢𝐝𝐞𝐨  
