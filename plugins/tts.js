ab90aa900c4204cdabab662a8e31f55300bf845b
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-04-25 02:09:02 +0200

    Update tts.js

--- plugins/tts.js
+++ plugins/tts.js
@@ -1,39 +1,55 @@
 import gtts from 'node-gtts'
-import { readFileSync, unlinkSync } from 'fs'
+import { readFileSync, unlinkSync, existsSync, mkdirSync } from 'fs'
 import { join } from 'path'
-const defaultLang = 'la'
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-let lang = args[0]
-let text = args.slice(1).join(' ')
-if ((args[0] || '').length !== 2) { 
-lang = defaultLang 
-text = args.join(' ')
+
+const defaultLang = 'it'
+
+let handler = async (m, { conn, args }) => {
+  let lang = args[0]
+  let text = args.slice(1).join(' ')
+
+  if ((args[0] || '').length !== 2) {
+    lang = defaultLang
+    text = args.join(' ')
+  }
+
+  if (!text && m.quoted?.text) text = m.quoted.text
+  if (!text) throw 'inserisci il testo'
+
+  try {
+    let res = await tts(text, lang)
+    await conn.sendFile(m.chat, res, 'tts.wav', null, m, true)
+  } catch (e) {
+    let res = await tts(text, defaultLang)
+    await conn.sendFile(m.chat, res, 'tts.wav', null, m, true)
+  }
 }
-if (!text && m.quoted?.text) text = m.quoted.text
-let res
-try { res = await tts(text, lang) }
-catch (e) {
-m.reply(e + '')
-text = args.join(' ')
-if (!text) throw `inserisci il testo`
-res = await tts(text, defaultLang)
-} finally {
-if (res) conn.sendFile(m.chat, res, 'tts.wav', null, m, true)
-}}
-handler.help = ['tts <lang> <teks>']
+
+handler.help = ['tts <lang> <text>']
 handler.tags = ['tools']
 handler.command = /^g?tts$/i
+
 export default handler
 
-function tts(text, lang = 'es') {
-console.log(lang, text)
-return new Promise((resolve, reject) => {
-try {
-let tts = gtts(lang)
-let filePath = join(global.__dirname(import.meta.url), '../tmp', (1 * new Date) + '.wav')
-tts.save(filePath, text, () => {
-resolve(readFileSync(filePath))
-unlinkSync(filePath)
-})
-} catch (e) { reject(e) }
-})}
+function tts(text, lang = 'it') {
+  return new Promise((resolve, reject) => {
+    try {
+      if (!existsSync('./tmp')) mkdirSync('./tmp')
+
+      let tts = gtts(lang)
+      let filePath = join('./tmp', Date.now() + '.wav')
+
+      tts.save(filePath, text, () => {
+        try {
+          const buffer = readFileSync(filePath)
+          unlinkSync(filePath)
+          resolve(buffer)
+        } catch (e) {
+          reject(e)
+        }
+      })
+    } catch (e) {
+      reject(e)
+    }
+  })
+}
