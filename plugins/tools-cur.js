940536c45d77c5d89cc700e9e5fffe7df6207588
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-05-02 17:08:48 +0200

    Update tools-cur.js

--- plugins/tools-cur.js
+++ plugins/tools-cur.js
@@ -7,7 +7,7 @@ import { fileURLToPath } from 'url'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
-const USERS_FILE = path.join(__dirname, '..', 'lastfm_users.json')
+const USERS_FILE = path.join(__dirname, 'storage', 'file-json', 'lastfm_users.json');
 
 if (!fs.existsSync(USERS_FILE)) fs.writeFileSync(USERS_FILE, '{}')
 
