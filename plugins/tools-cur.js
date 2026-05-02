7b738f2e4d738e6c6893fedac8a98f8b59d9254e
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-05-02 16:56:22 +0200

    Update tools-cur.js

--- plugins/tools-cur.js
+++ plugins/tools-cur.js
@@ -50,14 +50,25 @@ async function generateTrackImage(track) {
   const width = 600
   const height = 600
 
-  const imageUrl =
-    track.image?.find(img => img.size === 'extralarge')?.['#text'] ||
-    path.join(__dirname, '../icone/cur.jpg')
+  let imageUrl =
+    track.image?.find(i => i.size === 'extralarge')?.['#text'] ||
+    track.image?.find(i => i.size === 'large')?.['#text'] ||
+    track.image?.find(i => i.size === 'medium')?.['#text']
 
-  const img = await Jimp.read(imageUrl)
-  img.cover(width, height)
+  if (!imageUrl || imageUrl.trim() === '') {
+    imageUrl = path.join(__dirname, '../icone/cur.jpg')
+  }
 
-  return await img.getBufferAsync(Jimp.MIME_JPEG)
+  try {
+    const img = await Jimp.read(imageUrl)
+    img.cover(width, height)
+    return await img.getBufferAsync(Jimp.MIME_JPEG)
+  } catch (e) {
+    
+    const fallback = await Jimp.read(path.join(__dirname, '../icone/cur.jpg'))
+    fallback.cover(width, height)
+    return await fallback.getBufferAsync(Jimp.MIME_JPEG)
+  }
 }
 
 function normalizeJid(input) {
