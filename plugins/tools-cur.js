f161480c4360940a4516a3cf0328bb9886525772
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-05-03 07:06:57 +0200

    Update tools-cur.js

--- plugins/tools-cur.js
+++ plugins/tools-cur.js
@@ -115,8 +115,11 @@ async function generateTrackImageJimp(track) {
     track.image?.find(i => i.size === 'medium')?.['#text']
 
   if (!imageUrl || imageUrl.trim() === '') {
-    imageUrl = path.join(__dirname, '../icone/cur.jpg')
-  }
+  const fallbackPath = path.join(__dirname, '../icone/cur.jpg')
+  const imgBuffer = fs.readFileSync(fallbackPath)
+  const base64 = `data:image/jpeg;base64,${imgBuffer.toString('base64')}`
+  imageUrl = base64
+}
 
   try {
     const img = await Jimp.read(imageUrl)
