c02721b3e6bcc811c6e3ff2f32b8d670bd3512ce
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-05-07 01:55:13 +0200

    Update gp-hidetag.js

--- plugins/gp-hidetag.js
+++ plugins/gp-hidetag.js
@@ -1,12 +1,13 @@
-import { generateWAMessageFromContent, downloadContentFromMessage } from '@whiskeysockets/baileys'
+import { generateWAMessageFromContent } from '@whiskeysockets/baileys'
 import * as fs from 'fs'
 
 let handler = async (m, { conn, text, participants }) => {
   let users = participants.map(u => conn.decodeJid(u.id))
   let q = m.quoted ? m.quoted : m
   
-  let type = Object.keys(q.msg || q)[0] || ''
-  let msg = q.msg?.[type] || q.msg || q
+  let isViewOnce = q.msg?.viewOnce || q.viewOnce || q.mtype === 'viewOnceMessage' || q.mtype === 'viewOnceMessageV2'
+  let msg = isViewOnce ? q.msg.message[Object.keys(q.msg.message)[0]] : (q.msg || q)
+  let type = isViewOnce ? Object.keys(q.msg.message)[0] : q.mtype
 
   let captionText = m.quoted?.text ? `➠ ${m.quoted.text}` : (text?.trim() ? `➠ ${text.trim()}` : `➠`)
   let mentions = [...new Set([...(m.mentionedJid || []), ...users])]
@@ -18,11 +19,9 @@ let handler = async (m, { conn, text, participants }) => {
     }
 
     let media = await q.download?.().catch(() => null)
-    
-    let isViewOnce = q.msg?.viewOnce || q.viewOnce || false
-    let isGif = q.msg?.gifPlayback || q.gifPlayback || (q.mtype === 'videoMessage' && q.msg?.gifPlayback)
+    let isGif = msg?.gifPlayback || false
 
-    let commonOptions = {
+    let opt = {
       mentions: mentions,
       contextInfo: { 
         mentionedJid: mentions,
@@ -31,32 +30,31 @@ let handler = async (m, { conn, text, participants }) => {
       viewOnce: isViewOnce
     }
 
-    if (isGif) {
+    if (isGif || (type === 'videoMessage' && isGif)) {
       await conn.sendMessage(m.chat, { 
         video: media, 
         gifPlayback: true, 
         caption: captionText,
-        ...commonOptions 
+        ...opt 
       }, { quoted: m })
-    } else if (q.mtype === 'imageMessage' || type === 'imageMessage') {
-      await conn.sendMessage(m.chat, { image: media, caption: captionText, ...commonOptions }, { quoted: m })
-    } else if (q.mtype === 'videoMessage' || type === 'videoMessage') {
-      await conn.sendMessage(m.chat, { video: media, caption: captionText, ...commonOptions }, { quoted: m })
-    } else if (q.mtype === 'audioMessage' || type === 'audioMessage') {
+    } else if (type === 'imageMessage') {
+      await conn.sendMessage(m.chat, { image: media, caption: captionText, ...opt }, { quoted: m })
+    } else if (type === 'videoMessage') {
+      await conn.sendMessage(m.chat, { video: media, caption: captionText, ...opt }, { quoted: m })
+    } else if (type === 'audioMessage') {
       await conn.sendMessage(m.chat, { 
         audio: media, 
         mimetype: 'audio/mp4', 
-        ptt: q.msg?.ptt || false,
-        ...commonOptions 
+        ptt: isViewOnce ? true : (msg?.ptt || false),
+        ...opt 
       }, { quoted: m })
-    } else if (q.mtype === 'stickerMessage' || type === 'stickerMessage') {
-      await conn.sendMessage(m.chat, { sticker: media, ...commonOptions }, { quoted: m })
+    } else if (type === 'stickerMessage') {
+      await conn.sendMessage(m.chat, { sticker: media, ...opt }, { quoted: m })
     } else {
       await conn.sendMessage(m.chat, { text: captionText, mentions: mentions }, { quoted: m })
     }
   } catch (e) {
     console.error(e)
-    await conn.sendMessage(m.chat, { text: `❌ Errore: ${e.toString()}` }, { quoted: m })
   }
 }
 
