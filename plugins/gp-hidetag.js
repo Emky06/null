f7350fba3efb2f629204aa2e631c6ba98716dbce
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-05-07 01:11:15 +0200

    Update gp-hidetag.js

--- plugins/gp-hidetag.js
+++ plugins/gp-hidetag.js
@@ -4,7 +4,6 @@ import * as fs from 'fs'
 let handler = async (m, { conn, text, participants }) => {
   let users = participants.map(u => conn.decodeJid(u.id))
   let q = m.quoted ? m.quoted : m
-  let tagger = m.sender ? '@' + (m.sender.split('@')[0]) : ''
 
   let captionText
   if (m.quoted && m.quoted.text) {
@@ -13,6 +12,21 @@ let handler = async (m, { conn, text, participants }) => {
     captionText = `➠ ${text.trim()}`
   } else {
     captionText = `➠`
+  }
+
+  let mention
+  if (m.mentionedJid && m.mentionedJid.length > 0) {
+    mention = m.mentionedJid[0]
+    const userId = mention.split('@')[0].replace('+', '')
+    const mentionJid = userId + '@s.whatsapp.net'
+    
+    if (captionText.includes('@')) {
+      captionText = captionText.replace(/@\d+/g, `@${userId}`)
+    }
+    
+    var mentions = [mentionJid, ...users]
+  } else {
+    var mentions = users
   }
 
   try {
@@ -25,19 +39,18 @@ let handler = async (m, { conn, text, participants }) => {
       if (!media) throw 'Errore nel download del media'
 
       if (quoted.mtype === 'imageMessage') {
-        await conn.sendMessage(m.chat, { image: media, mentions: users, caption: captionText }, { quoted: m })
+        await conn.sendMessage(m.chat, { image: media, mentions: mentions, caption: captionText }, { quoted: m })
       } else if (quoted.mtype === 'videoMessage') {
-        await conn.sendMessage(m.chat, { video: media, mentions: users, caption: captionText, mimetype: 'video/mp4' }, { quoted: m })
+        await conn.sendMessage(m.chat, { video: media, mentions: mentions, caption: captionText, mimetype: 'video/mp4' }, { quoted: m })
       } else if (quoted.mtype === 'audioMessage') {
-        await conn.sendMessage(m.chat, { audio: media, mentions: users, mimetype: 'audio/mp4', fileName: `Hidetag.mp3` }, { quoted: m })
+        await conn.sendMessage(m.chat, { audio: media, mentions: mentions, mimetype: 'audio/mp4', fileName: `Hidetag.mp3` }, { quoted: m })
       } else if (quoted.mtype === 'stickerMessage') {
-        await conn.sendMessage(m.chat, { sticker: media, mentions: users }, { quoted: m })
+        await conn.sendMessage(m.chat, { sticker: media, mentions: mentions }, { quoted: m })
       }
     } else {
-      // testo semplice
       await conn.sendMessage(
         m.chat,
-        { text: captionText, mentions: users },
+        { text: captionText, mentions: mentions },
         { quoted: m }
       )
     }
