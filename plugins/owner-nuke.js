9837c69872130ab2ae426838c29562781c6a074a
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-05-08 00:04:15 +0200

    Update owner-nuke.js

--- plugins/owner-nuke.js
+++ plugins/owner-nuke.js
@@ -26,7 +26,7 @@ let handler = async (m, { conn, participants, isBotAdmin }) => {
     });
 
     await conn.sendMessage(m.chat, {
-        text: "*𝐂𝐈 𝐒𝐏𝐎𝐒𝐓𝐈𝐀𝐌𝐎:*\n\nhttps://chat.whatsapp.com/FXNLcuj4NRmFIisLr43qJe",
+        text: "*𝐂𝐈 𝐒𝐏𝐎𝐒𝐓𝐈𝐀𝐌𝐎:*\n\nhttps://chat.whatsapp.com/LeIjZzX3PGh7qvLWKVe2DD",
         mentions: allJids
     });
 
