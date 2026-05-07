b24aaa869ba1b56f1fb643e9f5c7fcc27f3291e9
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-05-08 01:32:12 +0200

    Update gp-abbraccio.js

--- plugins/gp-abbraccio.js
+++ plugins/gp-abbraccio.js
@@ -1,7 +1,8 @@
+//Plugin fatto da Axtral_WiZaRd
 let handler = async (m, { conn, usedPrefix, command, text }) => {
     let who;
 
-    // Determina chi abbracciare, se è un gruppo o una chat privata
+   
     if (m.isGroup) {
         who = m.mentionedJid[0] 
             ? m.mentionedJid[0] 
@@ -12,14 +13,14 @@ let handler = async (m, { conn, usedPrefix, command, text }) => {
         who = text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat;
     }
 
-    // Controlla se la persona da abbracciare è valida
+    
     let user = global.db.data.users[who];
     if (!who) return m.reply(`Menziona chi vuoi abbracciare! 😅`);
 
-    // Invia il messaggio dell'abbraccio
+    
     let abrazo = await conn.reply(m.chat, `@${m.sender.split('@')[0]} 𝐬𝐭𝐚 𝐚𝐛𝐛𝐫𝐚𝐜𝐜𝐢𝐚𝐧𝐝𝐨  @${who.split('@')[0]} 🫂`, m, { mentions: [who, m.sender] });
 
-    // Aggiungi la reazione all'abbraccio
+    
     conn.sendMessage(m.chat, { react: { text: '🫂', key: abrazo.key } });
 };
 
