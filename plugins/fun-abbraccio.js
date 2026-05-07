2ce65d4d630898b706dedfe98be7b8ac9fbf703e
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-05-08 01:32:31 +0200

    Create fun-abbraccio.js

---
+++ plugins/fun-abbraccio.js
@@ -0,0 +1,28 @@
+//Plugin fatto da Axtral_WiZaRd
+let handler = async (m, { conn, usedPrefix, command, text }) => {
+    let who;
+
+   
+    if (m.isGroup) {
+        who = m.mentionedJid[0] 
+            ? m.mentionedJid[0] 
+            : m.quoted ? m.quoted.sender 
+            : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' 
+            : false;
+    } else {
+        who = text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat;
+    }
+
+    
+    let user = global.db.data.users[who];
+    if (!who) return m.reply(`Menziona chi vuoi abbracciare! 😅`);
+
+    
+    let abrazo = await conn.reply(m.chat, `@${m.sender.split('@')[0]} 𝐬𝐭𝐚 𝐚𝐛𝐛𝐫𝐚𝐜𝐜𝐢𝐚𝐧𝐝𝐨  @${who.split('@')[0]} 🫂`, m, { mentions: [who, m.sender] });
+
+    
+    conn.sendMessage(m.chat, { react: { text: '🫂', key: abrazo.key } });
+};
+
+handler.command = ['abbraccia'];
+export default handler;
