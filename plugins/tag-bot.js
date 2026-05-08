2ecbcc5fa9e8a9fbe4c950304282c02e3118a5aa
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-05-08 06:47:51 +0200

    Update tag-bot.js

--- plugins/tag-bot.js
+++ plugins/tag-bot.js
@@ -1,32 +1,26 @@
-//tag Axtral
-
+//Plugin fatto da Axtral_WiZaRd
 let handler = m => m;
 
 handler.all = async function (m) {
   try {
-   
     if (m.sender === conn.user.jid) return;
 
-    const responses = {
-      '39351119848@s.whatsapp.net': '𝐂𝐇𝐄 𝐂𝐀𝐙𝐙𝐎 𝐕𝐔𝐎𝐈?',
+    const botNumber = conn.user.id.split(':')[0] + '@s.whatsapp.net';
 
+    const responses = {
+      [botNumber]: '𝐂𝐇𝐄 𝐂𝐀𝐙𝐙𝐎 𝐕𝐔𝐎𝐈?',
     };
 
-  
     let mentioned = m.mentionedJid || [];
-
     let taggedNumbers = mentioned.filter(jid => responses[jid]);
 
-    
     if (taggedNumbers.length === 0 || m.fromMe) return;
 
-  
     if (taggedNumbers.length === 1) {
       let responseMessage = responses[taggedNumbers[0]];
       await conn.reply(m.chat, responseMessage, m, { quoted: m });
-    } 
-    
-    
+    }
+
   } catch (error) {
     console.error('𝐄𝐫𝐫𝐨𝐫𝐞 ⚠️', error);
   }
