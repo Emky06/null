36b0d7ebeddd93aca81c14ed9061404128e5fa25
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-05-10 17:31:00 +0200

    Update admin-profilepic.js

--- plugins/admin-profilepic.js
+++ plugins/admin-profilepic.js
@@ -2,7 +2,7 @@
 let handler = async (m, { conn, text }) => {
     let who = m.mentionedJid?.[0] || m.quoted?.sender || m.sender;
 
-    // Verifica se l'utente target è il numero del bot
+    
     if (who === conn.user.jid) {
         await conn.sendMessage(m.chat, { 
             text: `🚫 Impossibile ottenere la foto profilo del bot.` 
@@ -10,7 +10,7 @@ let handler = async (m, { conn, text }) => {
         return;
     }
 
-    // Verifica se l'utente è uno degli owner
+    
     if (global.owner.some(o => (Array.isArray(o) ? o[0] : o) + '@s.whatsapp.net' === who)) {
         await conn.sendMessage(m.chat, {
             text: `🚫 Impossibile ottenere la foto profilo di un owner.`
@@ -19,14 +19,14 @@ let handler = async (m, { conn, text }) => {
     }
 
     try {
-        // Recupera la foto profilo dell'utente (se esiste)
+        
         let profilePicture = await conn.profilePictureUrl(who, 'image');
         await conn.sendMessage(m.chat, { 
             image: { url: profilePicture }, 
             caption: `📸` 
         }, { quoted: m, mentions: [who] });
     } catch (e) {
-        // Caso in cui l'utente non ha una foto profilo o non è disponibile
+        
         await conn.sendMessage(m.chat, { 
             text: `@${who.split('@')[0]} 𝐧𝐨𝐧 𝐡𝐚 𝐮𝐧𝐚 𝐟𝐨𝐭𝐨 𝐩𝐫𝐨𝐟𝐢𝐥𝐨 🚫`, 
             mentions: [who] 
