19cc0b4e8a89af9299fb09dba3b2cabc21a70370
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-05-14 03:36:24 +0200

    Update gp-rules.js

--- plugins/gp-rules.js
+++ plugins/gp-rules.js
@@ -1,11 +1,10 @@
 //Crediti: Onix, di Riad
 let handler = async (m, { conn }) => {
-    // Recupera le informazioni del gruppo
+    
     let groupMetadata = await conn.groupMetadata(m.chat);
-    let groupName = groupMetadata.subject; // Nome del gruppo
-    let groupDescription = groupMetadata.desc || '𝐃𝐞𝐬𝐜𝐫𝐢𝐳𝐢𝐨𝐧𝐞 𝐢𝐧𝐞𝐬𝐢𝐬𝐭𝐞𝐧𝐭𝐞 ⁉️'; // Descrizione del gruppo
+    let groupName = groupMetadata.subject; 
+    let groupDescription = groupMetadata.desc || '𝐃𝐞𝐬𝐜𝐫𝐢𝐳𝐢𝐨𝐧𝐞 𝐢𝐧𝐞𝐬𝐢𝐬𝐭𝐞𝐧𝐭𝐞 ⁉️'; 
 
-    // Messaggio di risposta con il nome del gruppo e la descrizione
     let infoMessage = `
 🟠 *𝐍𝐨𝐦𝐞 𝐝𝐞𝐥 𝐠𝐫𝐮𝐩𝐩𝐨:*
  ➪  ${groupName} \n
@@ -13,12 +12,11 @@ let handler = async (m, { conn }) => {
  ➪  ${groupDescription}
     `;
 
-    // Invia il messaggio con le informazioni
     await conn.sendMessage(m.chat, { text: infoMessage }, { quoted: m });
 };
 
-handler.command = /^(rules)$/i; // Comando
-handler.group = true; //solo nei gruppi
-handler.admin = true; //solo per admin
+handler.command = /^(rules)$/i; 
+handler.staff = true;
+handler.group = true; 
 
 export default handler;
