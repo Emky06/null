c1c6d524f699ab6df7b9125d0cc69357b3b008a2
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-05-14 03:44:18 +0200

    Create gp-regole.js

---
+++ plugins/gp-regole.js
@@ -0,0 +1,21 @@
+//Crediti: Onix, di Riad
+let handler = async (m, { conn }) => {
+
+    let groupMetadata = await conn.groupMetadata(m.chat);
+    let groupName = groupMetadata.subject; 
+    let groupDescription = groupMetadata.desc || '𝐃𝐞𝐬𝐜𝐫𝐢𝐳𝐢𝐨𝐧𝐞 𝐢𝐧𝐞𝐬𝐢𝐬𝐭𝐞𝐧𝐭𝐞 ⁉️'; 
+
+    let infoMessage = `
+🟠 *𝐍𝐨𝐦𝐞 𝐝𝐞𝐥 𝐠𝐫𝐮𝐩𝐩𝐨:*
+ ➪  ${groupName} \n
+🟡 *𝐃𝐞𝐬𝐜𝐫𝐢𝐳𝐢𝐨𝐧𝐞 𝐝𝐞𝐥 𝐠𝐫𝐮𝐩𝐩𝐨:*
+ ➪  ${groupDescription}
+    `;
+
+    await conn.sendMessage(m.chat, { text: infoMessage }, { quoted: m });
+};
+
+handler.command = /^(regole|rules)$/i; 
+handler.group = true; 
+
+export default handler;
