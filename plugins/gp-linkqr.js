e9c36e777e27b350c1d1d7934ac79137fc07bf2e
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-05-14 03:55:41 +0200

    Create gp-linkqr.js

---
+++ plugins/gp-linkqr.js
@@ -0,0 +1,21 @@
+//Plugin fatto da Axtral_WiZaRd
+import QRCode from 'qrcode';
+
+const handler = async (m, { conn }) => {
+    try {
+        const inviteLink = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(m.chat);
+        const qrImageBuffer = await QRCode.toBuffer(inviteLink);
+        await conn.sendMessage(m.chat, { image: qrImageBuffer, caption: 'Ecco il QR Code per il link del gruppo!' });
+    } catch (error) {
+        m.reply('Errore durante la generazione del QR Code: ' + error.message);
+    }
+};
+
+handler.help = ['linkgroup'];
+handler.tags = ['group'];
+handler.command = /^linkqr(gro?up)?$/i;
+handler.staff = true;
+handler.group = true;
+handler.botAdmin = true;
+
+export default handler;
