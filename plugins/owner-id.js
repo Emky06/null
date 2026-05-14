c7faa73358316a0450010cf1a75929ec12ba341b
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-05-14 03:54:33 +0200

    Create owner-id.js

---
+++ plugins/owner-id.js
@@ -0,0 +1,11 @@
+//Plugin fatto da Axtral_WiZaRd
+var handler = async (m, {conn, groupMetadata }) => {
+
+conn.reply(m.chat, `ⓘ 𝐋' 𝐢𝐝 𝐝𝐢 𝐪𝐮𝐞𝐬𝐭𝐨 𝐠𝐫𝐮𝐩𝐩𝐨 𝐞' ${await groupMetadata.id}`, m, )
+
+}
+handler.command = /^(id|gpid|gcid)$/i
+handler.group = true
+handler.owner = true
+
+export default handler
