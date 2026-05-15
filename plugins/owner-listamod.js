244d7c3525b16f73660340460b34965a54821311
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-05-15 15:50:45 +0200

    Update owner-listamod.js

--- plugins/owner-listamod.js
+++ plugins/owner-listamod.js
@@ -16,7 +16,7 @@ let handler = async (m, { conn }) => {
         const groupMetadata = await conn.groupMetadata(chatId).catch(() => null);
         const groupName = groupMetadata?.subject || 'Gruppo senza nome';
 
-text += `━━━━━━━━━━━━━━━━━━━`;
+text += `━━━━━━━━━━━━━━━━`;
 text += `\n● \`𝐆𝐫𝐮𝐩𝐩𝐨\` 💬\n`;
 text += `${groupName}\n`;
 text += `● \`𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐨𝐫𝐢\` 👮🏻‍♂️\n`;
