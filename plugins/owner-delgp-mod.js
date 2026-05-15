5a8ad20af9671efd8363313470455ffc1b468ae3
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-05-15 15:56:24 +0200

    Update owner-delgp-mod.js

--- plugins/owner-delgp-mod.js
+++ plugins/owner-delgp-mod.js
@@ -42,10 +42,10 @@ let handler = async (m, { command, text, usedPrefix }) => {
         let mods = data.prems || [];
 
         txt += `━━━━━━━━━━━━━━━━━━━\n`;
-        txt += `📌 \`𝐈𝐃 𝐆𝐫𝐮𝐩𝐩𝐨:`\\n${groupId}\n`;
+        txt += `📌 \`𝐈𝐃 𝐆𝐫𝐮𝐩𝐩𝐨:\`\n${groupId}\n`;
 
         if (mods.length) {
-          txt += `👮🏿‍♂️ 𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐨𝐫𝐢:\n`;
+          txt += `👮🏿‍♂️ \`𝐌𝐨𝐝𝐞𝐫𝐚𝐭𝐨𝐫𝐢:\`\n`;
           txt += mods.map(v => `● wa.me/${v}`).join('\n');
         } else {
           txt += '❌ 𝐍𝐞𝐬𝐬𝐮𝐧 𝐦𝐨𝐝𝐞𝐫𝐚𝐭𝐨𝐫𝐞';
