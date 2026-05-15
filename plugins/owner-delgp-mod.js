5ff4a2c7149a676bb61849b87415283bd5e6e5b7
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-05-15 15:53:23 +0200

    Update owner-delgp-mod.js

--- plugins/owner-delgp-mod.js
+++ plugins/owner-delgp-mod.js
@@ -41,7 +41,7 @@ let handler = async (m, { command, text, usedPrefix }) => {
         let data = groups[groupId];
         let mods = data.prems || [];
 
-        txt += `━━━━━━━━━━━━━━━━━━━`;
+        txt += `━━━━━━━━━━━━━━━━━━━\n`;
         txt += `📌 𝐈𝐃 𝐆𝐫𝐮𝐩𝐩𝐨:\n${groupId}\n`;
 
         if (mods.length) {
