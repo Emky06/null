d4fabe9c6c7197ce35748f67c2ad11f4276263d7
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-05-15 16:03:00 +0200

    Update owner-delgp-mod.js

--- plugins/owner-delgp-mod.js
+++ plugins/owner-delgp-mod.js
@@ -36,7 +36,7 @@ let handler = async (m, { command, text, usedPrefix }) => {
       }
 
       let txt = `╭━━━━━━━━━━━━━━━━━━━╮
-┃  📂 𝐃𝐀𝐓𝐀𝐁𝐀𝐒𝐄 𝐌𝐎𝐃 📂 ┃
+┃   📂 𝐃𝐀𝐓𝐀𝐁𝐀𝐒𝐄 𝐌𝐎𝐃 📂   ┃
 ╰━━━━━━━━━━━━━━━━━━━╯\n\n`;
 
       for (let groupId in groups) {
