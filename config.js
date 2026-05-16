5aea4344a9f7570cf949062490e97068f46199b4
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-05-16 05:11:35 +0200

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,6 @@ import { fileURLToPath } from 'url'
 global.botnumber = ""
 global.confirmCode = ""
 global.browserless = "2UUj5Hx30kQHUHZ661a9116c0763739fd8b80d2f1d1367ee8"
-global.googlekey = "AIzaSyDp0_0MT7ij2KrT6-KRq5tvlxss5MivFbQ"
-global.googleCX = "5021f5ef2ce92434a"
 
 global.owner = [
     ['17577575541', 'Owner', true],
