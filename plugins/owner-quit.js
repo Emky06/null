33527c04ce17751944eb6f0d00a5c87ce45dec72
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-05-16 22:49:46 +0200

    Update owner-quit.js

--- plugins/owner-quit.js
+++ plugins/owner-quit.js
@@ -1,33 +1,25 @@
 //Plugin fatto da Axtral_WiZaRd
-
-let handler = async (m, { conn, text, usedPrefix, command, isOwner }) => {
-  if (!isOwner) {
-    return m.reply('⚠️ Solo gli owner possono usare questo comando!');
-  }
-
+let handler = async (m, { conn, text }) => {
   const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
   let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i;
   let [_, code] = text.match(linkRegex) || [];
-  if (!code) throw `❌ Link non valido! Usa: ${usedPrefix + command} link_del_gruppo`;
+  if (!code) throw `❌ Link non valido!`;
 
-  m.reply(`Ok, esco da quel gruppo tra 3 secondi...`);
-
+  m.reply(`Esco fra 3 secondi, non cagarmi il cazzo per ora.`);
   await delay(3000);
 
   try {
-    let jid = await conn.groupAcceptInvite(code);
-    await conn.groupLeave(jid);
-    m.reply(`✅ Sono uscito dal gruppo correttamente.`);
+    let groupJid = await conn.groupAcceptInvite(code);
+    await conn.groupLeave(groupJid);
+
   } catch (e) {
-    console.log(e);
-    throw `⚠️ Non sono riuscito ad uscire dal gruppo.\nPossibile che il link non sia valido o sia scaduto.`;
+    throw `⚠️ Non riesco a uscire dal gruppo. Controlla il link o verifica che il bot sia dentro.`;
   }
 };
 
 handler.help = ['quit <chat.whatsapp.com>'];
 handler.tags = ['owner'];
 handler.command = ['quit'];
-handler.owner = true;
 
 export default handler;
