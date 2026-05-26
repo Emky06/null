aee70ed84f04e7442de26e6c322f24905f3d10df
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-05-27 01:59:34 +0200

    Update mod-muta.js

--- plugins/mod-muta.js
+++ plugins/mod-muta.js
@@ -23,12 +23,21 @@ const handler = async (msg, { conn, command, text }) => {
   const ownerJids = global.owner.map(o => o[0] + '@s.whatsapp.net');
 
   if (!mentionedJid) return conn.reply(chatId, `𝐓𝐚𝐠𝐠𝐚 𝐥\'𝐮𝐭𝐞𝐧𝐭𝐞 𝐝𝐚 ${command === 'muto' ? '𝐦𝐮𝐭𝐚𝐫𝐞 🔇' : '𝐬𝐦𝐮𝐭𝐚𝐫𝐞 🔊'}`, msg);
-  if (mentionedJid === groupOwner) throw '𝐈𝐥 𝐜𝐫𝐞𝐚𝐭𝐨𝐫𝐞 𝐝𝐞𝐥 𝐠𝐫𝐮𝐩𝐩𝐨 𝐧𝐨𝐧 𝐩𝐮𝐨̀ 𝐞𝐬𝐬𝐞𝐫𝐞 𝐦𝐮𝐭𝐚𝐭𝐨 ✘';
-  if (mentionedJid === botNumber) throw '𝐇𝐚𝐢 𝐚𝐩𝐩𝐞𝐧𝐚 𝐜𝐞𝐫𝐜𝐚𝐭𝐨 𝐝𝐢 𝐦𝐮𝐭𝐚𝐫𝐦𝐢? 𝐒𝐞𝐫𝐢𝐚𝐦𝐞𝐧𝐭𝐞? 🤡';
-  if (ownerJids.includes(mentionedJid)) throw '𝐍𝐨𝐧 𝐩𝐮𝐨𝐢 𝐦𝐮𝐭𝐚𝐫𝐞 𝐮𝐧 𝐨𝐰𝐧𝐞𝐫 ✘';
 
   const user = global.db.data.users[mentionedJid] || (global.db.data.users[mentionedJid] = {});
-  const isMute = command === 'muto';
+    const isMute = command === 'muta';
+
+if (isMute) {
+  if (mentionedJid === groupOwner)
+    throw '𝐈𝐥 𝐜𝐫𝐞𝐚𝐭𝐨𝐫𝐞 𝐝𝐞𝐥 𝐠𝐫𝐮𝐩𝐩𝐨 𝐧𝐨𝐧 𝐩𝐮𝐨̀ 𝐞𝐬𝐬𝐞𝐫𝐞 𝐦𝐮𝐭𝐚𝐭𝐨 ✘';
+
+  if (mentionedJid === botNumber)
+    throw '𝐇𝐚𝐢 𝐚𝐩𝐩𝐞𝐧𝐚 𝐜𝐞𝐫𝐜𝐚𝐭𝐨 𝐝𝐢 𝐦𝐮𝐭𝐚𝐫𝐦𝐢? 𝐒𝐞𝐫𝐢𝐚𝐦𝐞𝐧𝐭𝐞? 🤡';
+
+  if (ownerJids.includes(mentionedJid))
+    throw '𝐍𝐨𝐧 𝐩𝐮𝐨𝐢 𝐦𝐮𝐭𝐚𝐫𝐞 𝐮𝐧 𝐨𝐰𝐧𝐞𝐫 ✘';
+}
+
   const thumbnail = fs.readFileSync(`icone/${isMute ? 'muta.png' : 'smuta.png'}`);
   const miniaturaText = isMute ? '𝑼𝒕𝒆𝒏𝒕𝒆 𝒎𝒖𝒕𝒂𝒕𝒐 🔇' : '𝑼𝒕𝒆𝒏𝒕𝒆 𝒔𝒎𝒖𝒕𝒂𝒕𝒐 🔊';
 
