de468bf7d18aa6558600e0ec42d205c285e4d06b
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-06-01 12:08:58 +0200

    Update smarties-espelli.js

--- plugins/smarties-espelli.js
+++ plugins/smarties-espelli.js
@@ -65,7 +65,7 @@ if (ownerJids.includes(decodedMention)) return m.reply('> ⚠️ 𝐀𝐧𝐭�
   const senderTag = `@${m.sender.split`@`[0]}`
 
   const messaggio =
-`╭━━━〔 *𝐒𝐌𝐀𝐑𝐓𝐈𝐄𝐒 𝐊𝐈𝐋𝐋𝐄𝐑* 〕━━━╮
+`╭━━〔  *𝐒𝐌𝐀𝐑𝐓𝐈𝐄𝐒 𝐊𝐈𝐋𝐋𝐄𝐑*  〕━━╮
 ┃ ☠️ ${userTag} 𝐞̀ 𝐬𝐭𝐚𝐭𝐨 𝐞𝐥𝐢𝐦𝐢𝐧𝐚𝐭𝐨.
 ┃ 🩸 𝐍𝐞𝐬𝐬𝐮𝐧𝐚 𝐩𝐢𝐞𝐭𝐚̀ 𝐝𝐚 𝐩𝐚𝐫𝐭𝐞 𝐝𝐢 ${senderTag}
 ┃ ⚡ 𝐌𝐨𝐭𝐢𝐯𝐨: ${motivo}
