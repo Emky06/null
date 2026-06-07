a92d7dc2c4788f62adb09404ac7b73d2ce880f1c
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-06-07 18:32:15 +0200

    Update mod-muta.js

--- plugins/mod-muta.js
+++ plugins/mod-muta.js
@@ -22,10 +22,10 @@ const handler = async (msg, { conn, command, text }) => {
   const groupOwner = groupMetadata.owner || chatId.split('-')[0] + '@s.whatsapp.net';
   const ownerJids = global.owner.map(o => o[0] + '@s.whatsapp.net');
 
-  if (!mentionedJid) return conn.reply(chatId, `𝐓𝐚𝐠𝐠𝐚 𝐥\'𝐮𝐭𝐞𝐧𝐭𝐞 𝐝𝐚 ${command === 'muto' ? '𝐦𝐮𝐭𝐚𝐫𝐞 🔇' : '𝐬𝐦𝐮𝐭𝐚𝐫𝐞 🔊'}`, msg);
+  if (!mentionedJid) return conn.reply(chatId, `𝐓𝐚𝐠𝐠𝐚 𝐥'𝐮𝐭𝐞𝐧𝐭𝐞 𝐝𝐚 ${command === 'muto' ? '𝐦𝐮𝐭𝐚𝐫𝐞 🔇' : '𝐬𝐦𝐮𝐭𝐚𝐫𝐞 🔊'}`, msg);
 
-  const user = global.db.data.users[mentionedJid] || (global.db.data.users[mentionedJid] = {});
-    const isMute = command === 'muta';
+  const user = global.db.data.users[mentionedJid];
+  const isMute = command === 'muto';
 
 if (isMute) {
   if (mentionedJid === groupOwner)
@@ -52,27 +52,19 @@ if (isMute) {
   if (isMute) {
     if (user.muto) throw '𝐋`𝐮𝐭𝐞𝐧𝐭𝐞 𝐞̀ 𝐠𝐢𝐚̀ 𝐦𝐮𝐭𝐚𝐭𝐨!';
     user.muto = true;
-    return conn.sendMessage(
-      chatId,
-      {
-         text: `${userTag} 𝐞̀ 𝐬𝐭𝐚𝐭𝐨/𝐚 𝐦𝐮𝐭𝐚𝐭𝐨/𝐚 ✓ \n> 𝑺𝒐𝒍𝒐 𝒂𝒅𝒎𝒊𝒏 𝒆 𝒎𝒐𝒅𝒆𝒓𝒂𝒕𝒐𝒓𝒊 𝒑𝒐𝒔𝒔𝒐𝒏𝒐 𝒔𝒎𝒖𝒕𝒂𝒓𝒕𝒊.`,
-        mentions: [mentionedJid]
-      },
-      { quoted: fakeReply }
-    );
+    return conn.sendMessage(chatId, {
+      text: `${userTag} 𝐞̀ 𝐬𝐭𝐚𝐭𝐨/𝐚 𝐦𝐮𝐭𝐚𝐭𝐨/𝐚 ✓ \n> 𝑺𝒐𝒍𝒐 𝒂𝒅𝒎𝒊𝒏 𝒆 𝒎𝒐𝒅𝒆𝒓𝒂𝒕𝒐𝒓𝒊 𝒑𝒐𝒔𝒔𝒐𝒏𝒐 𝒔𝒎𝒖𝒕𝒂𝒓𝒕𝒊.`,
+      mentions: [mentionedJid]
+    }, { quoted: fakeReply });
   }
 
   if (!user.muto) throw '𝐋`𝐮𝐭𝐞𝐧𝐭𝐞 𝐞̀ 𝐠𝐢𝐚̀ 𝐬𝐦𝐮𝐭𝐚𝐭𝐨!';
   user.muto = false;
 
-  return conn.sendMessage(
-    chatId,
-    {
-      text: `${userTag} 𝐞̀ 𝐬𝐭𝐚𝐭𝐨/𝐚 𝐬𝐦𝐮𝐭𝐚𝐭𝐨/𝐚 ✓ \n> 𝑶𝒓𝒂 𝒓𝒊𝒏𝒈𝒓𝒂𝒛𝒊𝒂!`,
-      mentions: [mentionedJid]
-    },
-    { quoted: fakeReply }
-  );
+  return conn.sendMessage(chatId, {
+    text: `${userTag} 𝐞̀ 𝐬𝐭𝐚𝐭𝐨/𝐚 𝐬𝐦𝐮𝐭𝐚𝐭𝐨/𝐚 ✓ \n> 𝑶𝒓𝒂 𝒓𝒊𝒏𝒈𝒓𝒂𝒛𝒊𝒂!`,
+    mentions: [mentionedJid]
+  }, { quoted: fakeReply });
 };
 
 handler.command = /^(muto|smuto)$/i;
