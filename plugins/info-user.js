930f6e75c796b925c97c94c71fecc4c011e2e63a
Emky06 <134928023+Emky06@users.noreply.github.com>
2026-06-12 00:43:08 +0200

    Update info-user.js

--- plugins/info-user.js
+++ plugins/info-user.js
@@ -95,8 +95,8 @@ const animaliInfo = animaliCount > 0
     const grado = userData.grado || "Sfavillante";
 
     const messaggio = `╭── 📌 *𝐔𝐒𝐄𝐑 𝐈𝐍𝐅𝐎* 📌 ──╮\n` +
-      `👤 *𝐔𝐭𝐞𝐧𝐭𝐞:* ${nomeUtente}\n` +
-      /*`🏆 *𝐂𝐚𝐭𝐞𝐠𝐨𝐫𝐢𝐚:* ${categoria}\n` +*/
+      /*`👤 *𝐔𝐭𝐞𝐧𝐭𝐞:* ${nomeUtente}\n` +
+      `🏆 *𝐂𝐚𝐭𝐞𝐠𝐨𝐫𝐢𝐚:* ${categoria}\n` +*/
       `🔵 *𝐑𝐮𝐨𝐥𝐨:* ${ruolo}\n` + 
       `🔮 *𝐆𝐫𝐚𝐝𝐨:* ${grado}\n` +
       `📊 *𝐌𝐞𝐬𝐬𝐚𝐠𝐠𝐢:* ${userData.messaggi}\n` +
